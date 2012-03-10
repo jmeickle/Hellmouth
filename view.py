ed59d87c67519728409958b309b010bd78e1408e
Eronarn Palazzo <eronarn@gmail.com>
2012-03-10 18:41:55 -0500

    Basic hex support.

--- view.py
+++ view.py
@@ -15,6 +15,15 @@ class MainMap(Component):
         self.actors.append(actor)
 
     def draw(self):
+        # Draw the map. We do two tricks to make this hex-y:
+        #     * offset every other row by 1.
+        #     * multiply the x position by two.
+        for y in range(self.map.height):
+            for x in range(self.map.height):
+                offset = y % 2
+                self.window.addch(y, (2*x) + offset, self.map.cells[y][x])
+
+        # Draw the actors
         for actor in self.actors:
             self.window.addch(actor.y, actor.x, actor.glyph)
 #class MiniMap(Component):
