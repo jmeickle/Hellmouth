b2347a9e86ded3dc5c2730b7b6a40e27533d2b8e
Eronarn Palazzo <eronarn@gmail.com>
2012-03-11 10:04:16 -0400

    More hex code, and some pretty tests for it.

--- hexes.py
+++ hexes.py
@@ -16,4 +16,49 @@ def dist(x1, y1, x2, y2):
 
     return dist
 
-#def hex_iterator(x, y, rank):
+def hex_iterator(map, x, y, rank, curr=0):
+    mid = 10
+    print "Rank %d Hexagon:" % curr,
+    for Y in range(-curr, curr+1):
+        print ""
+        for X in range(-curr, curr+1):
+            if X+curr < x and y-curr > Y:
+                continue 
+            elif X-curr > x and y+curr < Y:
+                continue
+            elif dist(x, y, X, Y) != curr:
+                print "   -   ",
+                continue
+            else:
+                print "(%2d,%2d)" % (X, Y),
+                map[X+mid][Y+mid] = curr
+    print "\n"
+    if curr < rank:
+        hex_iterator(map, x, y, rank, curr+1)
+
+# Simple hex debugger
+map = []
+for y in range(0,20):
+    map.append([])
+    for x in range(0,20):
+        map[y].append(".")
+
+print "Array coordinates:"
+hex_iterator(map,0,0,4)
+
+import sys
+
+print "Distance in array representation:"
+for y in range(0,20):
+    for x in range(0,20):
+        sys.stdout.write(" %s"%map[y][x])
+    print "\n",
+print ""
+
+print "Distance in hex representation:"
+for y in range(0,20):
+    for spc in range(y):
+        sys.stdout.write(" ")
+    for x in range(0,20):
+        sys.stdout.write(" %s"%map[y][x])
+    print "\n",
