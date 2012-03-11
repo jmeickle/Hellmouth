a3cdcb40e3ae46a7c3d7c271ac5c90160a0331c9
Eronarn Palazzo <eronarn@gmail.com>
2012-03-10 20:53:49 -0500

    Move movement into a more general actor class.

--- actor.py
+++ actor.py
@@ -1,15 +1,10 @@
-from actor import Actor
-
-class Player(Actor):
+class Actor:
     def __init__(self):
-        Actor.__init__(self)
-        self.hp = 1
-        self.pos = 3, 3
-        self.glyph = '@'
-        self.map = None
+        self.glyph = 'x'
 
     def move(self, dir):
         pos = (self.pos[0]+dir[0], self.pos[1]+dir[1])
         if pos[0] >= 0 and pos[0] < self.map.width:
             if pos[1] >= 0 and pos[1] < self.map.height:
                 self.pos = pos
+
