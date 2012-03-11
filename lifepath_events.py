cfe547a1bf78d212d608028cfc392928778c1c94
Eronarn Palazzo <eronarn@gmail.com>
2012-03-11 14:39:34 -0400

    Revision of lifepath structure. Now I can start adding them!

--- lifepath_events.py
+++ lifepath_events.py
@@ -1,26 +1,59 @@
 # Contains all lifepath events.
+# 'age' (int)       : Age category the event fits into. Also broken up by comments.
 # 'name' (str)      : Display name during character generation. Defaults to key name.
 # 'text' (str)      : Display text during character generation.
 # 'short' (str)     : Alternate short text during character dumps.
 # 'effects' (dict)  : Dictionary of skill/advantage changes caused by the event.
 # 'years' (int)     : Number of years the event takes duirng character generation.
-# 'age' (int)       : Age category the event fits into.
 # 'choices' (tuple) : Tuple of choices for progression from this lifepath.
 eventdata = {
 # Start of data
+# Age 0: Parents (~-9 months old)
 'Wizards' : {
-    'name': "Wizards",
-    'text' : "Your parents were mighty wizards.",
-    'effects' : {'HP':5, 'ST':1},
-    'choices': ('Warriors', 'Hugs', 'Drugs'),
+    'age'     : 0,
+    'text'    :  "I was a child of a mighty wizard.",
+    'short'   : "Your parents were mighty wizards.",
+    'effects' : {'MP': 1, 'IQ': 2, 'Magery': 1},
+    'years'   : 1,
+    'choices' : ('', '', '', '', '', ''),
 },
-'Warriors' : {
-    'text' : "Your parents were fearsome warriors.",},
-'Hugs' : {
-    'text' : "You embraced the path of hugs.",},
-'Drugs' : {
-    'text' : "You embraced the path of drugs.",},
-'Warriors' : {
-    'text' : "Your parents were fearsome warriors.",},
+'Wizards' : {
+    'age'     : 0,
+    'text'    :  "I was a child of a mighty wizard.",
+    'short'   : "Your parents were mighty wizards.",
+    'effects' : {'MP': 1, 'IQ': 2, 'Magery': 1},
+    'years'   : 1,
+    'choices' : ('', '', '', '', '', ''),
+},
+'Wizards' : {
+    'age'     : 0,
+    'text'    :  "I was a child of a mighty wizard.",
+    'short'   : "Your parents were mighty wizards.",
+    'effects' : {'MP': 1, 'IQ': 2, 'Magery': 1},
+    'years'   : 1,
+    'choices' : ('', '', '', '', '', ''),
+},
+'Wizards' : {
+    'age'     : 0,
+    'text'    :  "I was a child of a mighty wizard.",
+    'short'   : "Your parents were mighty wizards.",
+    'effects' : {'MP': 1, 'IQ': 2, 'Magery': 1},
+    'years'   : 1,
+    'choices' : ('', '', '', '', '', ''),
+},
+# Age 1: Birth (0 years old)
+
+# Age 2: Early Childhood (~2 years old)
+
+# Age 3: Childhood (~8 years old)
+
+# Age 4: Teenager (~14 years old)
+
+# Age 5: Young Adult (~20 years old)
+
+# Age 6: Adult (>25 years old)
+
+# Age 7: Middle Age (>40 years old)
+# Not supported at this time
 # Close of data
 }
