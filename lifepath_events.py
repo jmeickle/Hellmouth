f55dcc940b2daca691525f74e2f16e6c5e3d9048
Eronarn Palazzo <eronarn@gmail.com>
2012-03-11 13:09:13 -0400

    More data fields.

--- lifepath_events.py
+++ lifepath_events.py
@@ -1,17 +1,18 @@
 # Contains all lifepath events.
-# 'name' (str)    : Display name during character generation. Defaults to key name.
-# 'text' (str)    : Display text during character generation.
-# 'short' (str)   : Alternate short text during character dumps.
-# 'effects' (dict): Dictionary of skill/advantage changes caused by the event.
-# 'years' (int)   : Number of years the event takes duirng character generation.
-# 'age' (int)     : Age category the event fits into.
-
+# 'name' (str)      : Display name during character generation. Defaults to key name.
+# 'text' (str)      : Display text during character generation.
+# 'short' (str)     : Alternate short text during character dumps.
+# 'effects' (dict)  : Dictionary of skill/advantage changes caused by the event.
+# 'years' (int)     : Number of years the event takes duirng character generation.
+# 'age' (int)       : Age category the event fits into.
+# 'choices' (tuple) : Tuple of choices for progression from this lifepath.
 eventdata = {
 # Start of data
 'Wizards' : {
     'name': "Wizards",
     'text' : "Your parents were mighty wizards.",
-    'effects' : {'HP':5, 'ST':1}
+    'effects' : {'HP':5, 'ST':1},
+    'choices': ('Warriors', 'Hugs', 'Drugs'),
 },
 'Warriors' : {
     'text' : "Your parents were fearsome warriors.",},
