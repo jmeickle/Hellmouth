da449e22dab6ae8ccb60defa3211a0814b78dd48
Eronarn Palazzo <eronarn@gmail.com>
2012-03-11 12:24:28 -0400

    Lifepath event data

---
+++ lifepath_events.py
@@ -0,0 +1,16 @@
+# Contains all lifepath events.
+# 'name' (str)    : Display name during character generation. Defaults to key name.
+# 'text' (str)    : Display text during character generation.
+# 'dump' (str)    : Alternate text during character dumps.
+# 'effects' (dict): Dictionary of skill/advantage changes caused by the event.
+# 'years (int)    : Number of years the event takes duirng character generation.
+
+eventdata = {
+# Start of data
+'Wizards' : {
+    'name': "~Wizards~",
+    'text' : "Your parents were mighty wizards.",},
+'Warriors' : {
+    'text' : "Your parents were fearsome warriors.",},
+# Close of data
+}
