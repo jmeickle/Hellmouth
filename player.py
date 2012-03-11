b1761755687d822d317574313a2a1ffef8ae5de2
Eronarn Palazzo <eronarn@gmail.com>
2012-03-10 20:38:08 -0500

    Remove some unused variables and add a move function based on the directions defined in hellmouth.py

--- player.py
+++ player.py
@@ -1,6 +1,12 @@
 class Player:
     def __init__(self):
         self.hp = 1
-        self.x = 3
-        self.y = 3
+        self.pos = 3, 3
         self.glyph = '@'
+        self.map = None
+
+    def move(self, dir):
+        pos = (self.pos[0]+dir[0], self.pos[1]+dir[1])
+        if pos[0] >= 0 and pos[0] < self.map.width:
+            if pos[1] >= 0 and pos[1] < self.map.height:
+                self.pos = pos
