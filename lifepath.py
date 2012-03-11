ed3ca6aef64852858426d61be08f7a3402e6ae04
Eronarn Palazzo <eronarn@gmail.com>
2012-03-11 12:06:55 -0400

    More lifepath stuff.

--- lifepath.py
+++ lifepath.py
@@ -13,20 +13,30 @@ class Lifepath:
 
     def display(self):
         for event in self.events:
-            print event
+            event.display()
 
 class LifepathEvent:
-    def __init__(self, parent=None):
+    def __init__(self, choice, parent=None):
+        choices = {
+        'Wizards' : {
+            'name': "Hello",
+            'text' : "Your parents were mighty wizards.",},
+        'Warriors' : {
+            'text' : "Your parents were fearsome warriors.",},
+        }
+        self.choice = choices.get(choice)
+
         # Tree structure: parent, choice, and options.
         self.parent = parent
         self.child = None
         self.options = None
 
-        # Actual effects of the event.
-        self.name = "Debug event."
-        self.text = "Debug text."
-        self.effects = {'Combat Reflexes' : 1}
+        # Display information for the event.
+        self.name = self.choice.get('name', choice) # Defaults to the search key.
+        self.text = self.choice.get('text', 'Missing description.')
 
+        # Actual gameplay effects.
+        self.effects = self.choice.get('effects', 'No gameplay effects.')
 
     def choose(self, event):
         self.child = LifepathEvent(self)
@@ -38,9 +48,9 @@ class LifepathEvent:
 
 def lifepath_test():
     lifepath = Lifepath()
-    lifepath.start(LifepathEvent())
-    lifepath.initial.display()
+    lifepath.start(LifepathEvent('Warriors'))
     lifepath.display()
 
 if __name__ == "__main__":
     lifepath_test()
+
