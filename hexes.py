198d9969fc09da200ca996e29da3ecc293641894
Eronarn Palazzo <eronarn@gmail.com>
2012-03-11 11:17:54 -0400

    More hex testing stuff.

--- hexes.py
+++ hexes.py
@@ -16,49 +16,67 @@ def dist(x1, y1, x2, y2):
 
     return dist
 
-def hex_iterator(map, x, y, rank, curr=0):
-    mid = 10
-    print "Rank %d Hexagon:" % curr,
+def hex_iterator(map, x, y, rank, debug=False, curr=0):
+    if debug is True:
+        print "Rank %d Hexagon:" % curr,
     for Y in range(-curr, curr+1):
-        print ""
+        if debug is True:
+            print ""
         for X in range(-curr, curr+1):
-            if X+curr < x and y-curr > Y:
-                continue 
-            elif X-curr > x and y+curr < Y:
-                continue
-            elif dist(x, y, X, Y) != curr:
-                print "   -   ",
-                continue
-            else:
-                print "(%2d,%2d)" % (X, Y),
-                map[X+mid][Y+mid] = curr
-    print "\n"
+            if debug is True:
+                if dist(x, y, x+X, y+Y) < curr:
+                    print "   -   ",
+                elif dist(x, y, x+X, y+Y) > curr:
+                    print "   +   ",
+            if dist(x, y, x+X, y+Y) == curr:
+                if debug is True:
+                    print "(%2d,%2d)" % (X, Y),
+                map[x+X][y+Y] = curr
+    if debug is True:
+        print "\n"
     if curr < rank:
-        hex_iterator(map, x, y, rank, curr+1)
+        hex_iterator(map, x, y, rank, debug, curr+1)
 
-# Simple hex debugger
-map = []
-for y in range(0,20):
-    map.append([])
-    for x in range(0,20):
-        map[y].append(".")
+# Simple tool to test hex code.
+# Takes a center point, hex size, and map size.
+def hex_tester(x, y, rank, height, width):
 
-print "Array coordinates:"
-hex_iterator(map,0,0,4)
+    print "Rank %s Hexagon at %s,%s; Map %sx%s" % (rank,x,y,width,height)
 
-import sys
+    # Generate empty map.
+    map = []
+    for Y in range(0,height):
+        map.append([])
+        for X in range(0,width):
+            map[Y].append(".")
 
-print "Distance in array representation:"
-for y in range(0,20):
-    for x in range(0,20):
-        sys.stdout.write(" %s"%map[y][x])
-    print "\n",
-print ""
+    print "Array coordinates:"
+    hex_iterator(map,x,y,rank,debug=True)
 
-print "Distance in hex representation:"
-for y in range(0,20):
-    for spc in range(y):
-        sys.stdout.write(" ")
-    for x in range(0,20):
-        sys.stdout.write(" %s"%map[y][x])
-    print "\n",
+    print "Distance in array representation:"
+    for Y in range(0,height):
+        for X in range(0,width):
+            sys.stdout.write(" %s"%map[Y][X])
+        print "\n",
+
+    print ""
+
+    print "Distance in hex representation:"
+    for Y in range(0,height):
+        for spaces in range(Y):
+            sys.stdout.write(" ")
+        for X in range(0,width):
+            sys.stdout.write(" %s"%map[Y][X])
+        print "\n",
+
+if __name__ == "__main__":
+    import sys
+#    import getopt
+#    options = getopt.getopt(sys.argv[1:], 'xyrhw')
+    x = 10
+    y = 10
+    rank = 8
+    height = 24
+    width = 24
+#    hex_tester(0,0,4,20,20)
+    hex_tester(x, y, rank, height, width)
