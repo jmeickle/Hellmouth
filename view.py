7a89562e80e936a0b6c4b1c0470736d7f961b7cc
Eronarn Palazzo <eronarn@gmail.com>
2012-03-11 08:28:42 -0400

    Large changes to some functions in here.

--- view.py
+++ view.py
@@ -1,11 +1,13 @@
+viewrange = 10
+
 class Component():
 
-    def __init__(self, window, x=0, y=0):
-        self.window = window
-        self.x = x
-        self.y = y
+    def __init__(self, window, x, y, startx, starty):
+        self.window = window.subwin(y, x, starty, startx)
+        self.x = startx
+        self.y = starty
 
-    # Rectangular drawing function.
+    # Rectangular character function.
     def rd(self, x, y, glyph):
         self.window.addch(y, x, glyph)
 
@@ -13,42 +15,61 @@ class Component():
     def rds(self, x, y, string):
         self.window.addstr(y, x, string)
 
-    # Hex drawing function.
+    # Hex character function.
     def hd(self, x, y, glyph):
         self.window.addch(y, 2*x + y, glyph)
 
 class MainMap(Component):
-    def __init__(self, window, x, y):
-        Component.__init__(self, window, x, y)
+    def __init__(self, window, x, y, startx, starty):
+        Component.__init__(self, window, x, y, startx, starty)
         self.map = None
         self.actors = []
+        self.player = None
+
+    def center_x(self):
+        return self.player.pos[0]
+
+    def center_y(self):
+        return self.player.pos[1]
+
+    # Accepts viewrange offsets to figure out what part of the map is visible.
+    def get_glyph(self, x, y):
+        # DEBUG:
+        # x = min(self.map.height-1, x+self.center_x())
+        # y = min(self.map.height-1, y+self.center_y())
+        # return chr(48 + x % 10)
+        x += self.center_x()
+        y += self.center_y()
+        return self.map.cells[min(self.map.height-1, y)]\
+                             [min(self.map.width-1, x)]
 
     def add(self, actor):
         self.actors.append(actor)
 
     def draw(self):
-        # Draw the map.
-        for y in range(self.map.height):
-            for x in range(self.map.height):
-# DEBUG:
-#                self.hd(x+1, y+1, chr(48+x))
-                self.hd(x, y, self.map.cells[y][x])
+        # The +1 is so that @ = 0, 0 in viewrange.
+        for y in range(-viewrange, viewrange+1):
+            for x in range(-viewrange, viewrange+1):
+                self.hd(viewrange+x, viewrange+y, self.get_glyph(x, y))
 
         # Draw the actors
         for actor in self.actors:
-            self.hd(actor.pos[0], actor.pos[1], actor.glyph)
+            self.hd(viewrange+1+actor.pos[0]-self.center_x(),\
+                    viewrange+1+actor.pos[1]-self.center_y(),\
+                    actor.glyph)
+
         self.window.refresh()
 
 class Stats(Component):
-    def __init__(self, window, x, y):
-        Component.__init__(self, window, x, y)
+    def __init__(self, window, x, y, startx, starty):
+        Component.__init__(self, window, x, y, startx, starty)
 
     def draw(self):
-        self.rds(self.x, self.y, "HP: 5")
-        self.rds(self.x, self.y+2, "ST: 15")
-        self.rds(self.x, self.y+3, "DX: 15")
-        self.rds(self.x, self.y+4, "IQ: 15")
-        self.rds(self.x, self.y+5, "HT: 15")
+        self.rds(0, 1, "HP: 5")
+        self.rds(0, 2, "ST: 15")
+        self.rds(0, 3, "DX: 15")
+        self.rds(0, 4, "IQ: 15")
+        self.rds(0, 5, "HT: 15")
 
 #class MiniMap(Component):
 
