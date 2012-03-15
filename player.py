b86a262b440f72a7d874f65189a8cfd977fa2421
Eronarn Palazzo <eronarn@gmail.com>
2012-03-15 11:12:48 -0400

    Minor changes

--- player.py
+++ player.py
@@ -2,7 +2,6 @@ from actor import Actor
 
 class Player(Actor):
     def __init__(self):
-        Actor.__init__(self, 0, 0)
-        self.hp = 1
+        Actor.__init__(self)
         self.glyph = '@'
         self.map = None
