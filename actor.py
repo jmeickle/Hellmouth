7a0a5307d8cc05c62f8d216428c93c89bd1c26f7
Eronarn Palazzo <eronarn@gmail.com>
2012-03-15 11:07:29 -0400

    Some changes to actor code.

--- actor.py
+++ actor.py
@@ -1,22 +1,44 @@
 class Actor:
-    def __init__(self, x, y, glyph='x'):
-        self.glyph = glyph
-        self.pos = (x, y)
+    def __init__(self):
+        # Descriptive information
+        self.glyph = 'x'
+        self.color = 'red'
+        self.name = 'Buggy monster'
+        self.description = 'This is the description'
 
+        # More 'permanent' game info: stats, skills, etc.
+        self.body = BodyPlan(self, 'humanoid')
         self.stats = {}
         self.traits = {}
         self.skills = {}
+
+        # More 'volatile' information: inventory, effects, etc.
+        self.pos = None
         self.inventory = {}
-        self.bodyplan = BodyPlan(self, 'humanoid')
+        self.effects = {}
 
-    def stat(stat):
-        stats.get(stat)
+    # Change actor coords.
+    def go(pos):
+        self.pos = pos
 
+    # Try to move and return whether it worked.
     def move(self, dir):
         pos = (self.pos[0]+dir[0], self.pos[1]+dir[1])
+        if self.valid_move(pos):
+            self.go(pos)
+            return True
+        else:
+            return False
+
+    # Check move validity.
+    def valid_move(self, pos):
         if pos[0] >= 0 and pos[0] < self.map.width:
             if pos[1] >= 0 and pos[1] < self.map.height:
-                self.pos = pos
+                return True
+
+    # Retrieve actor stats.
+    def stat(stat):
+        stats.get(stat)
 
 class BodyPlan:
     def __init__(self, parent, type):
