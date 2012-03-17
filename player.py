d8d5112388807db4d18c6320ca530f4eeba3a4ab
Eronarn Palazzo <eronarn@gmail.com>
2012-03-17 17:56:21 -0400

    Set up HP for actors

--- player.py
+++ player.py
@@ -6,3 +6,37 @@ class Player(Actor):
         self.glyph = '@'
         self.name = 'Player'
         self.color = 'cyan-black'
+
+# NOT THE PLAYER BUT GOES IN HERE ANYWAYS
+
+class MeatSlave(Actor):
+    def __init__(self):
+        Actor.__init__(self)
+        self.glyph = '@'
+        self.name = 'meat slave'
+        self.color = 'yellow-black'
+        self.hp = 1
+
+class MeatGolem(Actor):
+    def __init__(self):
+        Actor.__init__(self)
+        self.glyph = '8'
+        self.name = 'meat golem'
+        self.color = 'blue-black'
+        self.hp = 50
+
+class MeatWorm(Actor):
+    def __init__(self):
+        Actor.__init__(self)
+        self.glyph = '~'
+        self.name = 'meat worm'
+        self.color = 'magenta-black'
+        self.hp = 20
+
+class MeatHydra(Actor):
+    def __init__(self):
+        Actor.__init__(self)
+        self.glyph = 'D'
+        self.name = 'meat hydra'
+        self.color = 'magenta-black'
+        self.hp = 80
