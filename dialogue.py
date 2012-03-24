c40fc196b1f5a79df8b84cfc1eb2b63a13f77bfc
Eronarn Palazzo <eronarn@gmail.com>
2012-03-24 12:20:50 -0400

    Dialogue storage file.

---
+++ dialogue.py
@@ -0,0 +1,11 @@
+chargen = {
+    "age-0" : "Tell me, what of your parents?",
+    "age-1" : "And your birth?",
+    "age-2" : "What of your early childhood?",
+    "age-3" : "Later in childhood, how did you fare?",
+    "age-4" : "How about your teenage years?",
+    "age-5" : "What of your years as a young adult?",
+    "age-6" : "How did life find you as an adult?",
+    "age-7" : "What changed?",
+    "age-8" : "Why did you choose to become a demonslayer?"
+}
