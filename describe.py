3c6ac3b0644a36609a273b599845b299ced5ea48
Eronarn Palazzo <eronarn@gmail.com>
2012-03-23 22:12:07 -0400

    Mostly cleanup.

--- describe.py
+++ describe.py
@@ -1,66 +1,88 @@
 import copy
 import re
+import random
+
+# This file is mostly for generating random text. Other stuff will eventually be moved out of it.
+
+# Key combinations. Supports anything, but using these keys:
+
+# 'dmg': Damage text.
+# (AMT): none, scratch, injure, cripple, sever, kill, overkill
+# (TYPE): See define.py for a list of possible damage types.
+# (SKILL): Margin of success. low, med, high, crit
+# (LOC): For specialcasing messages (e.g., decapitation)
+
+# If a tuple is provided. Tuples can be nested.
 
 class Descriptions():
     dict = {
-    "dmg-sever-cut-neck" : "decapitate",
+    "dmg" : ("hit", "strike"),
+    "dmg-crit" : ("skillfully", "gracefully"),
+    "dmg-burn" : "burn",
+    "dmg-cut" : "cut",
+    "dmg-cr" : "whack",
+    "dmg-pi" : "stab",
+    "dmg-imp" : "impale",
+#    "dmg-scratch-cut" : "cut",
+#    "dmg-scratch-cut" : "cut",
+#    "dmg-scratch-cut" : "cut",
+#    "dmg-scratch-cut" : "cut",
+#    "dmg-injure-cut" : "
+#    "dmg-injure-cut" : "
+#    "dmg-injure-cut" : "
+#    "dmg-injure-cut" : "
+#    "dmg-cripple-cut"
+#    "dmg-cripple-cut"
+#    "dmg-cripple-cut"
+#    "dmg-cripple-cut"
+    "dmg-sever-cut" : "lop off",
     "dmg-cripple-cut-neck" : "partially decapitate",
+    "dmg-sever-cut-neck" : "decapitate",
     "dmg-sever-crush-neck" : "crush the windpipe",
-    "dmg-cut" : "slice",
-    "dmg-pierce" : "stab",
-    "dmg-impale" : "spear",
-    "dmg-crush" : "whack",
-    "dmg" : "hit",
-    "dmg-crit" : "skillfully hit",
 }
     hits = {}
     fail = {}
 
 def base_stat(value):
+    if value > 20: return "godly"
+    elif value > 14: return "amazing"
+    elif value > 12: return "exceptional"
+    elif value > 10: return "above-average"
+    elif value is 10: return "average"
+    elif value > 7: return "below-average"
+    elif value is 7: return "poor"
+    else: return "crippling"
+
+# Utility functions:
+
+# Convert a list of keys into a dash-separated string.
+def build_key(keys):
     str = ""
-
-    if value > 20:
-        str = "godly"
-    elif value > 14:
-        str = "amazing"
-    elif value > 12:
-        str = "exceptional"
-    elif value > 10:
-        str = "above-average"
-    elif value is 10:
-        str = "average"
-    elif value > 7:
-        str = "below-average"
-    elif value is 7:
-        str = "poor"
-    else:
-        str = "crippling"
-
-    return str
-
-#def damage_amount(value):
-
-#def size(value):
-
-#def reach(value):
-
-def build(terms):
-    str = ""
-    for term in terms:
-        if term != "":
+    for key in keys:
+        if key != "":
             if str == "":
-                str += term
+                str += key
             else:
-                str += "-%s" % term
-#    print str
+                str += "-%s" % key
     return str
 
-# Get a string from parts
+# Hit the database to find a given string.
 def g(str):
     Descriptions.hits[str] = True
-    return Descriptions.dict.get(str, None)
+    ret = Descriptions.dict.get(str, None)
+    if ret is not None:
+        if isinstance(ret, tuple):
+            ret = pick(ret)
+    return ret
 
-# Get whether the string failed already
+# Choose a random option from the tuple, and keep going if it's another tuple.
+def pick(options):
+    choice = random.choice(options)
+    if isinstance(choice, tuple):
+        choice = pick(choice)
+    return choice
+
+# Hit the database to find whether a given string failed already
 def f(str):
     return Descriptions.fail.get(str, None)
 
@@ -71,47 +93,55 @@ def fail(str):
     else:
         Descriptions.fail[str] += 1
 
+# String building functions
+
+# Shorthand for main 'describe' function.
 def d(str):
     return describe(str)
 
+# Convert a string into relevant text. Anything between @ or ~ is tokenized.
+# TODO: Keep @/~ and update the db keys to include them?
 def describe(str):
     ret = ""
     for substr in re.split('([@~]\S*[@~])', str):
         if re.search('[@~]', substr) is not None:
-            # TODO: Keep @/~ and update the db to include them?
             ret += replace(re.sub('[@~]', '', substr))
         else:
             ret += substr
     return ret
 
-def replace(str):
-    exact = g(str)
+# Replace a dash-separated key with text from the database.
+def replace(key):
+    exact = g(key)
     if exact is not None:
         return exact
     else:
-        terms = re.split('-',str)
-#        print "TERMS: %s" % terms
-        ret = None
-        for i in range(len(terms)):
-            ret = indexed_remove(terms, i, ret)
+        ret = key
+        keys = re.split('-',key)
+        for i in range(len(keys)):
+            ret = indexed_remove(keys, i, ret)
         return ret
 
-def indexed_remove(terms, n, ret):
-#    work = terms[:]
-#    print "len terms: %s" % len(terms)
+# If an exact match wasn't found, recursively remove keys from the list of keys
+# to try to find a partial match using this pattern:
+#
+# "one-two-three-four" -> "one-two-three", "one-two-four", "one-three-four", "one-two", "one"
+#
+# If no match for any of those permutations is found, the original string is used.
+def indexed_remove(keys, n, ret):
     # The indices to be possibly removed in this pass:
-#    print [x for x in reversed(range(len(terms)-n, len(terms)))]
-    for x in reversed(range(len(terms)-n, len(terms))):
-        copy = terms[:]
+    for x in reversed(range(len(keys)-n, len(keys))):
+        copy = keys[:]
         copy[x]=""
-        string = build(copy)
-        # DEBUG: Change this to 'is None' and it'll stop checking already-failed.
-        if f(string) is not False:
-            query = g(string)
-            if query is None:
-                fail(string)
+        key = build_key(copy)
+        # DEBUG: Change this to 'is None' (vs. 'is not False') and it'll stop checking already-failed.
+        if f(key) is not False:
+            result = g(key)
+            if result is None:
+                fail(key)
             else:
-                ret = query
+                # Return early to avoid looking for worse matches.
+                ret = result
                 return ret
 
     if n > 1:
