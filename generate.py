7a1c79e059f5b1c798fe9714e699a25b1e2613f5
Eronarn Palazzo <eronarn@gmail.com>
2012-05-17 10:29:57 -0400

    Improvements to generation & generation test code.

--- generate.py
+++ generate.py
@@ -1,13 +1,34 @@
 # Generate (or improve) an actor's points.
 import random
+from dice import _3d6, _d6, roll
 
-def spend_points(actor, points=None):
-    # If we didn't feed in a number of points, we're using the actor's starting points.
+def spend_points(actor, generator, points=None):
+    # If we didn't feed in a number of points, we're using the actor's
+    # starting points. The alternative is that we're improving the actor.
     if points is None:
-        points = actor.points
+        points = actor.points["total"]
+
+    # Dict of expenditures thus far
     spent = {}
-    while points > 0:
-        points -= 1
+
+    # Number of tries, to avoid infinite loops
+    tries = 0
+    allowed = 50
+
+    while points > 0 and tries < allowed:
+        tries += 1
+        choice, cost = generator.choose()
+        if cost <= points:
+            # TODO: continue out here, based on logic of skill appropriateness.
+            if spent.get(choice) is None:
+                spent[choice] = cost
+            else:
+                spent[choice] += cost
+            points -= cost
+
+    # Save any unspent points.
+    spent["unspent"] = points
+    return spent
 
 #Tasks:
 # 1) Improve existing skills
@@ -16,28 +37,29 @@ def spend_points(actor, points=None):
 class Generator:
     def __init__(self, choices={}):
         self.choices = choices # See test code for an example of structure.
-        self.default = 100 # Default weight.
+        self.amount = _d6 # Default points per choice made.
+        self.weight = 100 # Default weight.
 
     # Pick from the weighted list(s).
     def choose(self, current=None):
         if current is None:
             current = self.choices
         choice = None
-        sum = reduce(lambda x, y: x+y.get("weight", self.default), current.values(), 0)
+        sum = reduce(lambda x, y: x+y.get("weight", self.weight), current.values(), 0)
         weighting = random.randint(1, sum)
 
         for k, v in current.items():
-            weighting -= v.get("weight", self.default)
+            weighting -= v.get("weight", self.weight)
             if weighting <= 0:
                 options = v.get("options")
                 if options is None:
-                    return k
+                    return k, v.get("amount", self.amount())
                 else:
                     return self.choose(options)
 
 # Test code.
 if __name__ == "__main__":
-    print "Random actor generation test:"
+    print "Random choice test:"
     generator = Generator()
 
     # Weight defaults to 100, as set above.
@@ -66,4 +88,55 @@ if __name__ == "__main__":
 
     print "==CHOICES=="
     for choice in choices:
-        print "- %s" % choice
+        print "- %s (%s points)" % choice
+
+    print ""
+
+    print "Random actor generation test:"
+
+    # Import necessary classes.
+    from actor import Actor
+
+    # Generate a dummy actor
+    actor = Actor()
+    actor.points["total"] = 103
+
+    # Spend points!
+    spent = spend_points(actor, generator)
+
+    # Store spending:
+    actor.points["skills"] = spent
+
+    for choice, points in actor.points["skills"].items():
+        print "%s - %s points" % (choice, points)
+    print "-"*20
+    print "SUM: %s" % sum(actor.points["skills"].values())
+
+    print ""
+
+    print "Random actor improvement test:"
+
+    runs = 3
+    for run in range(runs):
+        print ""
+
+        # Choose number of points.
+        points = _d6()+10+actor.points["skills"].pop("unspent", 0)
+
+        print "Improved actor by %s points." % points
+        print ""
+
+        # Spend points!
+        spent = spend_points(actor, generator, points)
+
+        # Merge in prior spending:
+        for k,v in spent.items():
+            if actor.points["skills"].get(k) is None:
+                actor.points["skills"][k] = v
+            else:
+                actor.points["skills"][k] += v
+
+        for choice, points in actor.points["skills"].items():
+            print "%s - %s points" % (choice, points)
+        print "-"*20
+        print "SUM: %s" % sum(actor.points["skills"].values())
