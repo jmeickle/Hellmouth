f5389e4808d62d3ee058a3d834fadf7a8f587ac4
Eronarn Palazzo <eronarn@gmail.com>
2012-05-24 19:42:39 -0400

    Help file (also start screen here for now.)

---
+++ help.py
@@ -0,0 +1,34 @@
+entry = {
+"start-meat" : "\
+As the daze of the teleport spell fades away, you realize that you are not \
+where you - nor anyone - would want to be. Towering walls of meat glisten in \
+the dim bioluminescent light. A side of beef sloughs off the nearest one as \
+you watch. The scent of <red-black>blood</> is heavy in the air, and your boots sink into \
+the ground-beef ominously.\
+<br>\
+<br>\
+This is a grisly, gristly place. You must escape.",
+
+"commands" : "\
+Commands: \
+<br>\
+<br>\
+<green-black>Enter</> can mean activate, confirm, or accept, whereas \
+<green-black>Space</> means cancel or go back.\
+<br>\
+<br>\
+You can move your character with the <green-black>hex keys</> on your number \
+pad. These keys are also used for navigating hexagonal menus.\
+<br>\
+<br>\
+The <green-black>arrow keys</> are used to scroll lists and navigate \
+rectangular menus. You can usually use these keys even while taking actions \
+with your character.\
+<br>\
+<br>\
+Press <green-black>'v'</> to view your surroundings. You can press \
+<green-black>Enter</> while viewing a creature to see its character sheet.\
+<br>\
+<br>\
+Press <red-black>'Ctrl+q'</> at any time to quit the game. (There is no prompt!)",
+}
