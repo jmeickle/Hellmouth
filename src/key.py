3107c4fb2a6c7a9155e5a4384b117662b445961d
Eronarn Palazzo <eronarn@gmail.com>
2012-05-25 18:52:26 -0400

    Revisions to keyin file.

--- src/key.py
+++ src/key.py
@@ -1,37 +1,11 @@
-# Define dict of keys/actions
+# TODO: Define a dict of keys/actions for remappable keys.
 from define import *
-from hex import *
 
 # Return the corresponding control-modified key.
 def ctrl(c):
     return ord(c) - 96
 
-def keyin(stdscr, views):
-        # Keyin stuff for player actions
-        c = stdscr.getch()
-
-        # Always allow help.
-#        if c == ctrl('?'):
-#            views.append(HelpScreen() ... & etc.
-
-        # Always allow quitting.
-        if c == ctrl('q'):
-            exit()
-#            exit(views[0].player.__dict__)
-
-        # Offer keyin to each view, and continue if any of them returns false
-        for view in views:
-            if view._keyin(c) is False:
-                return False
-
-        # 'Global' keypresses that work anywhere
-        if c == ord('P'):
-            views[0].player.attack(views[0].player)
-        elif c == ord('g'):
-            views[0].player.get_all()
-        elif c == ord('d'):
-            views[0].player.drop_all()
-
+# Return the corresponding hex direction.
 def hexkeys(c):
     if c == ord('7'):
         return NW
@@ -48,14 +22,3 @@ def hexkeys(c):
     elif c == ord('5'):
         return CC
     return None
-
-# TODO: Convert this into the multiple menu code
-#        elif hasattr(chargen.selector, 'parent') is True:
-#            if c == curses.KEY_RIGHT:
-#                chargen.selector.next()
-#            elif c == curses.KEY_LEFT:
-#                chargen.selector.prev()
-#            elif c == curses.KEY_ENTER or c == ord('\n'):
-#                chargen.selector.choose()
-#                views.remove(chargen)
-
