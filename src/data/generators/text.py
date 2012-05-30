b5216613048978bbd8d7434ea45382d282f5b4b0
Eronarn Palazzo <eronarn@gmail.com>
2012-05-30 19:51:54 -0400

    Text generation data in the right file now.

---
+++ src/data/generators/text.py
@@ -0,0 +1,31 @@
+tokens = {
+    "dmg" : ("hit with a @weapon@", "strike with your @weapon@"),
+    "dmg-crit" : ("skillfully", "gracefully"),
+    "dmg-burn" : "burn",
+    "dmg-cut" : "cut",
+    "dmg-cut-swing" : ("chop", "slice"),
+    "dmg-cr" : "whack",
+    "dmg-cr-punch" : ("smack", "punch", "hit", "whack"),
+    "dmg-pi" : "stab",
+    "dmg-imp" : "impale",
+    "dmg-imp-thrust" : ("stab", "skewer"),
+#    "dmg-scratch-cut" : "cut",
+#    "dmg-scratch-cut" : "cut",
+#    "dmg-scratch-cut" : "cut",
+#    "dmg-scratch-cut" : "cut",
+#    "dmg-injure-cut" : "
+#    "dmg-injure-cut" : "
+#    "dmg-injure-cut" : "
+#    "dmg-injure-cut" : "
+#    "dmg-cripple-cut"
+#    "dmg-cripple-cut"
+#    "dmg-cripple-cut"
+#    "dmg-cripple-cut"
+    "dmg-sever-cut" : "lop off",
+    "dmg-cripple-cut-neck" : "partially decapitate",
+    "dmg-sever-cut-neck" : "decapitate",
+    "dmg-sever-crush-neck" : "crush the windpipe",
+    "miss" : "miss",
+    "def" : "defends against",
+
+}
