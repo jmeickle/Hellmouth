6bd2f3f90d5d0cf4e458240c9bcc57a71b49e2e9
Eronarn Palazzo <eronarn@gmail.com>
2012-05-30 19:06:14 -0400

    Add armor to random generation.

--- src/maps/encounter.py
+++ src/maps/encounter.py
@@ -68,7 +68,7 @@ class Encounter:
             # TODO: Replace this test code with something better.
             if random.randint(1, 10) == 1:
                 for x in range(random.randint(1, 3)):
-                    cell.put(generate_item(random.choice(("shortsword", "spear", "thrusting broadsword"))))
+                    cell.put(generate_item(random.choice(("shortsword", "spear", "thrusting broadsword", "armor", "gloves", "boots", "leggings"))))
 
             self.cells[pos] = cell
 
