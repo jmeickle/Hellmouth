d6bc6d0bc213dc0a3e647a7fe1d30497e010611e
Eronarn Palazzo <eronarn@gmail.com>
2012-05-31 17:21:34 -0400

    Use levelname and mapname consistently rather than trying to figure it out ourselves in the view.

--- src/views/tactical.py
+++ src/views/tactical.py
@@ -258,8 +258,8 @@ class Status(View):
         View.__init__(self, window, x, y, start_x, start_y)
 
     def draw(self):
-         self.line(self.map.name, "red-black")
-         self.line("Level %s" % self.map.level.current_depth())
+         self.line(self.map.level.name, "red-black")
+         self.line(self.map.name)
 #        self.line("Pain", "red-black")
 #        self.line("Shock", "magenta-black")
 
