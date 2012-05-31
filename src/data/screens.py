383aeeab2c951ade3eee4a64054db44a70278436
Eronarn Palazzo <eronarn@gmail.com>
2012-05-30 22:00:52 -0400

    Meat screens

--- src/data/screens.py
+++ src/data/screens.py
@@ -12,22 +12,12 @@ you watch. The scent of <red-black>blood</> is heavy in the air, and your boots 
 the ground-beef ominously.\
 <br>\
 <br>\
-This is a grisly, gristly place. You must escape.",
-},
-
-"meat-1" : {
-    "header_left" : "<red-black>Welcome to the Meat Arena!</>",
-    "body_text" : "\
-As the daze of the teleport spell fades away, you realize that you are not \
-where you - nor anyone - would want to be. Towering walls of meat glisten in \
-the dim bioluminescent light. A side of beef sloughs off the nearest one as \
-you watch. The scent of <red-black>blood</> is heavy in the air, and your boots sink into \
-the ground-beef ominously.\
+This is a grisly, gristly place. You must escape.\
 <br>\
 <br>\
-This is a grisly, gristly place. You must escape.",
+Unfortunately, the staircase leading upwards has been stuffed with stuffing. \
+The only way you can go is down.",
 },
-
 "meat-2" : {
     "header_left" : "<red-black>A second serving</>",
     "body_text" : "\
@@ -36,16 +26,91 @@ sauce from your brow. You almost slip a few times in the dim light, but the \
 passage soon brightens again - you must be near an exit.\
 <br>\
 <br>\
-Or an entrance. The gaping cavity at the end of the tunnel opens into another \
-level, much like the first. It looks like you'll have to keep looking for an \
-exit from this hellish place!",
+Or an entrance.\
+<br>\
+<br>\
+The gaping cavity at the end of the tunnel opens into another \
+level much like the first. It looks like you'll have to keep looking for an \
+exit from this bizarre place!",
+},
+"meat-3" : {
+    "header_left" : "<red-black>The Grand Gate</>",
+    "body_text" : "\
+Even before you round the stairs, it is clear that this level is different \
+from the last. You hear neither the chattering of meat slaves nor the roaring \
+of hydras. In fact, apart from the ever-present gurgle of uneasy digestion, it \
+is totally silent.\
+<br>\
+<br>\
+It takes your eyes a few moments to adjust to the dimmer light. You've exited \
+the staircase into a straight corridor lined by pillars. At the other side is \
+a tremendous gate built from the spine of a blue whale - that must be the exit! \
+<br>\
+<br>\
+Unfortunately, it seems to be guarded. A tall humanoid creature composed of \
+truly prodigious amounts of meat stands at attention. It quickly notices your \
+presence and bellows a challenge.\
+<br>\
+<br>\
+<red-black>\"WHO DISTURBS THE SAUCEROR?\"</>\
+<br>\
+<br>\
+The guard raises its maul and charges brazenly down the corridor at you.",
+
 },
 
-"meat-end" : {
-    "header_left" : "<red-black>It all looks the same...</>",
+"meat-4" : {
+    "header_left" : "<red-black>The taste of power</>",
     "body_text" : "\
-You've sliced apart many enemies, but they haven't stopping coming. The stench \
-of spoiled meat is overpowering. You must escape - but you can't. No one can.",
+As you pass through the gate, you come to understand the reason for its \
+construction. The meat beyond it is older - less tender, to be sure, but far \
+wiser. You shiver with anticipation as you imagine the great power that must \
+run through the veins of such a place.\
+<br>\
+<br>\
+Then you remember that this primal meat was sealed away by something powerful \
+enough that even the Meat Commander saw fit to swear fealty to it. Again, you \
+shiver.",
+},
+
+"meat-5" : {
+    "header_left" : "<red-black>What saucery is this?</>",
+    "body_text" : "\
+The gurgling of the walls intensifies as you walk down this set of stairs. It \
+becomes hotter, too, and you find yourself sweating almost as much as the \
+ceiling. You are not comforted when you find the source of the noiset: large \
+pipes of intestine that bulge from the floor and spew sauce into ever-growing \
+pools. \
+<br>\
+<br>\
+As you get closer to the sauce vats, you notice that they are boiling. \
+Apparently, they are the source of the heat - stepping into them is probably \
+<red-black>dangerous</>.",
+},
+
+"meat-6" : {
+    "header_left" : "<red-black>The Tower of the Sauceror</>",
+    "body_text" : "\
+A single massive kielbasa stabs through the roast beef at the center of this \
+cavern. It must be the source of the sauce, for it radiates an awful heat, and \
+large sections of intestinal tubing exit its casing to enter into the meaty \
+walls.\
+<br>\
+<br>\
+A dark-robed figure stands in front of the towering kielbasa. It traces arcane \
+glyphs with sausage fingers while chanting words in the dark meat speech. The \
+very air drips with eldritch grease.\
+<br>\
+<br>\
+This must be the home of the Sauceror that the Meat Commander spoke of. It is \
+clearly a being both insanely powerful and powerfully insane.",
+},
+"meat-end" : {
+    "header_left" : "<red-black>...</>",
+    "body_text" : "\
+The Sauceror is dead. You've sliced apart many enemies, but they haven't \
+stopping coming. The stench of spoiled meat is overpowering. You must escape \
+- but you can't. No one can.",
 },
 
 }
