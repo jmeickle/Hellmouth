aca5f385b752ea460e06e5288b1012733fa81af2
Eronarn Palazzo <eronarn@gmail.com>
2012-05-30 22:57:02 -0400

    Prevent crashing when failing to find terrain to use

--- src/actors/player.py
+++ src/actors/player.py
@@ -16,7 +16,8 @@ class Player(Actor):
     # Use stairs that you are standing on.
     def stairs(self):
         stairs = self.map.terrain(self.pos)
-        return self.interact(stairs)
+        if stairs is not None:
+            return self.interact(stairs)
 
     # Interact with terrain.
     # TODO: Flags, etc.
