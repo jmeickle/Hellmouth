126662698c8c509794e12d08a4461141184f6e92
Eronarn Palazzo <eronarn@gmail.com>
2012-05-31 20:02:06 -0400

    Comment out these asserts for now. The try/catch should be fine.

--- src/views/view.py
+++ src/views/view.py
@@ -58,7 +58,7 @@ class View(Component):
         if box is True:
             draw_x += self.LEFT
             draw_y += self.TOP
-        assert self.undrawable((draw_x, draw_y)) is False, "rd function tried to draw '%s' out of bounds: %s at %s." % (glyph, self.__dict__, (draw_x, draw_y))
+        #assert self.undrawable((draw_x, draw_y)) is False, "rd function tried to draw '%s' out of bounds: %s at %s." % (glyph, self.__dict__, (draw_x, draw_y))
         try: self.window.addch(draw_y, draw_x, glyph, self.attr(col, attr))
         except curses.error: pass
 
@@ -70,7 +70,7 @@ class View(Component):
         if box is True:
             draw_x += self.LEFT
             draw_y += self.TOP
-        assert self.undrawable((draw_x, draw_y)) is False, "rds function tried to draw '%s' out of bounds: %s at %s." % (string, self.__dict__, (draw_x, draw_y))
+        #assert self.undrawable((draw_x, draw_y)) is False, "rds function tried to draw '%s' out of bounds: %s at %s." % (string, self.__dict__, (draw_x, draw_y))
         try: self.window.addstr(draw_y, draw_x, string, self.attr(col, attr))
         except curses.error: pass
 
