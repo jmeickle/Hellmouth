4cb9f91bd52492ed7393640f64ee246524a82e7e
Eronarn Palazzo <eronarn@gmail.com>
2012-06-08 14:50:55 -0400

    Forgot to add the data file for the generator!

---
+++ src/data/generators/equipment.py
@@ -0,0 +1,43 @@
+class Meat:
+    def func(self):
+        return False
+class Cheap:
+    def func(self):
+        return False
+
+generators = {
+
+"default_armor" : {
+#    "full" : {"weight" : 50},
+#    "body" : {},
+    "partial" : {"options" : {
+        "torso" : {
+            "material" : Meat,
+            "quality" : Cheap,
+            "items" : ["breastplate"],
+        },
+        "upper body" : {
+            "weight" : 50,
+            "material" : Meat,
+            "quality" : Cheap,
+            "items" : ["breastplate", "arms", "gauntlets"],
+        },
+    }}, #</partial>
+},
+"default_weapons" : {
+    "long swords" : { "weight" : 10, "options" : {
+        "broadsword" : { "options" : {
+            "broadsword" : {},
+            "thrusting broadsword" : {"weight" : 50},
+        }},
+        "bastard sword" : { "weight" : 50, "options" : {
+            "bastard sword" : {},
+            "thrusting bastard sword" : {"weight" : 50},
+        }},
+        "greatsword" : { "weight" : 20, "options" : {
+            "greatsword" : {},
+            "thrusting greatsword" : {"weight" : 50},
+        }},
+    }}, #</long swords>
+}
+}
