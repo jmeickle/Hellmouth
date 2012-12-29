d59411815db9cdfae564de9dc1674943a1a96d33
Eronarn Palazzo <eronarn@gmail.com>
2012-12-29 18:12:40 -0500

    Large restructuring of actions and action primitives.

--- src/lib/actors/action.py
+++ src/lib/actors/action.py
@@ -23,87 +23,183 @@
 # for most game, though these can be overridden if necessary.
 
 # Hack to allow running this script as a single file.
+# TODO: Remove.
 if __name__ == '__main__':
     import sys
     sys.path.insert(0, '/home/eronarn/Code/Hellmouth/')
 
-from src.lib.util.dynamic import *
-
+# An individual action primitive.
 class ActionPrimitive():
+    # When a primitive is initialized, it generates a class method for each
+    # of its self.methods() according to the pattern in self.add_method(). By
+    # default, these generated class methods call a corresponding actor method
+    # according to the pattern in self.apply().
     def __init__(self):
         for method in self.methods():
             self.add_method(method)
 
-    # The actor methods that this primitive will call.
+    # The methods that can be applied to this primitive.
+    # TODO: Make this a dict containing arguments to check against.
     def methods(self):
-        return ['can', 'attempt', 'cancel']
+        return ['believe', 'can', 'attempt', 'cancel']
 
-    # The name of the corresponding actor method for this primitive.
+    # The name of a corresponding actor method for this primitive. By default
+    # it looks for "believe_touch", "can_grasp", etc.
     def apply(self, method):
         return method + '_' + self.__class__.__name__
 
-    # Add checks for actor methods to the primitive's class.
+    # Add a class method to a primitive to check the corresponding actor method.
     def add_method(self, method):
-        def _default_method(self, actor, target):
-            return getattr(actor, self.apply(method))(target)
+        def _default_method(self, actor, *args):
+            return getattr(actor, self.apply(method))(*args)
         _default_method.__name__ = method
         setattr(self.__class__, _default_method.__name__, _default_method)
 
+# A chain of multiple action primitives.
 class Action():
-    def __init__(self, primitives):
-        self.primitives = primitives
+    # When an action is initialized, it retrieves its definition.
+    def __init__(self, action_key):
+        self.definition = actiondict[action_key]
 
-    # Check a given method for each primitive in the chain.
-    def check(self, method, actor, target):
-        for primitive in self.primitives:
-            current = globals()[primitive]()
-            if getattr(current, method)(actor, target) is False:
-                print "Failed at %" % primitive
-                return False 
-            else:
-                print "%s->%s_%s->%s" % (actor.name, method, current.__class__.__name__, target.name)
-        return True
+    # Process a list of methods for each primitive in this action's
+    # definition. The necessary arguments for each primitive are pulled from
+    # the provided keyword arguments. The return value accumulates the method
+    # return values in a 'striped' list:
+    #
+    # [Method1, Method2, Method3] x [Primitive1, Primitive2, Primitive3]
+    # == M1P1, M2P1, M3P1, M1P2, M2P2, M3P2, M1P3, M2P3, M3P3
+    #
+    # If any function returns False, processing will stop, meaning that the
+    # return value has variable length.
+    def process(self, methods, **kwargs):
+        results = []
+        # For each primitive in this action's definition...
+        for primitive_definition in self.definition:
+
+            # Get the name of the current primitive.
+            primitive = primitive_definition[0]
+
+            # Get the primitive's desired arguments from the action definition.
+            primitive_args = primitive_definition[1:len(primitive_definition)]
+
+            # Always use actor as the first argument.
+            args = (kwargs["actor"],)
+
+            # Populate the rest of the arguments, if any.
+            for primitive_arg in primitive_args:
+                # We don't do this here - what if an argument really *should*
+                # be none? Leave it up to the function to assert.
+                # assert(kwargs.get(primitive_arg) is not None)
+                args += (kwargs.get(primitive_arg),)
+
+            # Process each method of the current primitive.
+            for method in methods:
+                result = self.process_primitive(method, primitive, *args)
+
+                # Handle the case of pure T/F function returns.
+                # TODO: Make all reachable functions return better data rather
+                # than just T/F.
+                try:
+                    if result[0]:
+                        pass
+                except TypeError:
+                    result = (result,)
+
+                # Store the function result.
+                results.append(result)
+
+                # If the function's primary result was False, exit early.
+                if result[0] is False:
+                    return results
+        return results
+
+    # Process a single method for a single primitive.
+    # TODO: Restructure this.
+    def process_primitive(self, method, primitive, *args):
+        return getattr(globals()[primitive](), method)(*args)
+
+#
+# ACTION PRIMITIVES:
+#
+
+# Typically, these can take either a single actor or item as target:
 
 # Touch the target (with anything).
 class touch(ActionPrimitive): pass
 
-# Hold on to the target using a manipulator.
+# Hold on to the target.
 class grasp(ActionPrimitive): pass
 
-# Let go of a target held in a manipulator.
+# Let go of the target.
 class ungrasp(ActionPrimitive): pass
 
-# Move the target from one location to another.
-class move(ActionPrimitive): pass
+# Hold the target outward from your body.
+class ready(ActionPrimitive): pass
 
-# Hold the target outward from your body using a manipulator.
-class wield(ActionPrimitive): pass
-
-# Attach the target to your body.
-class wear(ActionPrimitive): pass
-
-# Activate the target.
-class use(ActionPrimitive): pass
-
-# Get the item into inventory.
-class get(ActionPrimitive): pass
-
-# Drop the target from inventory.
-class drop(ActionPrimitive): pass
+# Hold the target at the side of your body.
+class unready(ActionPrimitive): pass
 
 # Throw the target at another target.
 class throw(ActionPrimitive): pass
 
-# Swing a sword:
-#action = ["touch", "grasp", "wield", "use"]
+# Use the target.
+class use(ActionPrimitive): pass
 
-# Throw a grenade:
-#action = ["touch", "grasp", "wield", "throw"]
+# Attach the target to your body.
+class wear(ActionPrimitive): pass
 
-# Throw a grenade (with pin):
-#action = ["touch", "grasp", "wield", "use", "throw"]
+# Unattach the target from your body.
+class unwear(ActionPrimitive): pass
+
+# Typically, these will require a single item as target:
+
+# Store the target into inventory.
+class store(ActionPrimitive): pass
+
+# Drop the target from inventory.
+class unstore(ActionPrimitive): pass
+
+# Typically, these will require a single actor or item as a first target, and a
+# location as a second target:
+
+# Move the target from one map location to another.
+class move(ActionPrimitive): pass
+
+# Typically, these will require a single actor or item as a first target, and a
+# single item as a second target:
+
+# Touch the target (with a specific item).
+class contact(ActionPrimitive): pass
+
+# Point a readied target at a second target.
+# n.b. - It is up to the item whether brandishing is compatible with readying!
+class brandish(ActionPrimitive): pass
+
+# Stop pointing a readied target at a second target.
+# n.b. - This can have a second target because you can pointedly lower your
+# weapon 'at' someone, e.g., if asked to by a guard.
+class unbrandish(ActionPrimitive): pass
+
+# Use an item at the target.
+class use_at(ActionPrimitive): pass
+
+#
+# ACTIONS:
+#
+
+# Dictionary of defined actions.
+actiondict = {
+    "attack" : (
+        ("touch", "item"),
+        ("grasp", "item"),
+        ("ready", "item"),
+        ("contact", "target", "item"),
+        ("use_at", "target", "item")
+    ),
+}
 
 # Basic testing code.
+# TODO: Deprecated!
 if __name__ == '__main__':
 #    test = grasp()
 #    print "Class methods:", test.__class__.__dict__
