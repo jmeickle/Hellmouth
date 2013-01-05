1f6ae2c9bc8ff0938672640dafde44b694b26f9b
Eronarn Palazzo <eronarn@gmail.com>
2013-01-05 10:03:40 -0500

    Make the initial debug statement use the DEBUG() function.

--- src/lib/util/debug.py
+++ src/lib/util/debug.py
@@ -3,7 +3,6 @@
 import logging
 
 logging.basicConfig(filename='hellmouth.log',level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s',)
-logging.debug('Imported debug.py and initialized logging.')
 
 # TODO: Use this somewhere.
 debug_frequency = {}
@@ -13,3 +12,5 @@ def DEBUG(message, level=logging.DEBUG):
     hits = debug_frequency.get(message, 0) + 1
     debug_frequency[message] = hits
     logging.debug("%s:%s" % (hits, message))
+
+DEBUG('Imported debug.py and initialized logging.')
