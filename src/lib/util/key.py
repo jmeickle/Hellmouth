a32a86122f46e5761bbcba7a791217f42eb5bad9
Eronarn Palazzo <eronarn@gmail.com>
2013-01-05 10:11:57 -0500

    CMD_{SAVE/LOAD/QUIT} and some comment/spacing/spelling fixes.

--- src/lib/util/key.py
+++ src/lib/util/key.py
@@ -2,10 +2,16 @@
 from define import *
 from hex import *
 
+# Game process commands
+CMD_SAVE = "save the game"
+CMD_LOAD = "load the game"
+CMD_QUIT = "quit the game"
+
+# Player character commands
 CMD_ATTACK = "attack"
 CMD_TALK = "talk"
-CMD_HEX = "hex directions"
-CMD_RECT = "rectangular directions"
+CMD_HEX = "hex direction"
+CMD_RECT = "rectangular direction"
 CMD_CONFIRM = "confirm or submit"
 CMD_CANCEL = "cancel or go back"
 
@@ -15,16 +21,21 @@ commands[CMD_TALK] = ("t",)
 commands[CMD_HEX] = ("1", "3", "4", "6", "7", "9", "5")
 commands[CMD_CANCEL] = (' ',)
 
+# Return whether the provided keypress belongs to the provided command.
 def cmd(c, command):
     if c <= 256:
         c = chr(c)
     if c in commands[command]:
         return True
     return False
+
 # Return the corresponding control-modified key.
 def ctrl(c):
     return ord(c) - 96
 
+# Hack to prevent help screen recursion.
+# You won't be able to quit the game while on a help screen, though.
+# TODO: Replace with something better! Yikes.
 globals = {}
 globals[ord("?")] = True
 globals[ctrl("q")] = True
