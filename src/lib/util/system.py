b39d0014653c35c0a3770fef7e11127348aaed99
Eronarn Palazzo <eronarn@gmail.com>
2013-01-13 14:43:02 -0500

    Helper function to get folder names.

--- src/lib/util/system.py
+++ src/lib/util/system.py
@@ -10,3 +10,9 @@ def makedir(path):
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
+
+# Get a list of directories inside of a path
+def folders(path=".", pattern=None):
+    for filename in os.listdir(path):
+        if os.path.isdir(path + "/" + filename):
+            yield filename
