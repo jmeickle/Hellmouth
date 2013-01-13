d9169b24315bab930bb511e2d88f8f71f804c66b
Eronarn Palazzo <eronarn@gmail.com>
2013-01-13 14:38:07 -0500

    Stub main/info files for Hellmouth.

---
+++ src/games/hellmouth/info.py
@@ -0,0 +1,6 @@
+# The name of this game.
+name = "Hellmouth"
+# The version number.
+version = "vNULL"
+# The description
+description = "The real game."
---
+++ src/games/hellmouth/main.py
@@ -0,0 +1,5 @@
+from src.lib.components.component import Component
+
+# TODO: Split this into a generic game class and a meatgame meatclass
+class Game(Component):
+    pass
