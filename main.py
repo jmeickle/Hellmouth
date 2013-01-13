109bb60092b63ea70ddc51da3d7856669f500d9c
Eronarn Palazzo <eronarn@gmail.com>
2013-01-13 14:49:42 -0500

    Rewrite of how games get initialized in main.py.

--- main.py
+++ main.py
@@ -1,45 +1,82 @@
-import curses
 import sys
 import getopt
 from src.lib.util.debug import *
+from src.lib.util.define import *
+from src.lib.util import system
 
-# Main function, which is called below.
-def main(stdscr):
-    try:
-        DEBUG("Parsing command line arguments.")
-        options, remainder = getopt.getopt(sys.argv[1:],"a:b:")
-    except getopt.GetoptError:
-        DEBUG("Failed to parse arguments: %s." % sys.argv[1:])
-        exit()
+# Set up a dictionary of game settings.
+arguments = {}
 
-    # TODO: Handle command line arguments.
-    for opt, arg in options:
-        DEBUG("Option '%s': '%s'" % (opt, arg))
+# Set a bitmask for game-wide display modes.
+displayflags = {
+    "silent" : 0b1,
+    "curses" : 0b10,
+    "unicode" : 0b100,
+    "256" : 0b1000,
+}
 
-        # The game to be run
-        if opt in ('-g', '--game'):
-            pass
-        # The save to be loaded
-        elif opt in ('-s', '--savefile'):
-            pass
+# Default to displaying with curses
+arguments["displaymode"] = displayflags["curses"]
 
-    # Make the cursor invisible.
-    curses.curs_set(0)
-    # Use raw input mode.
-    curses.raw()
+# Default to launching Meat Arena
+# TODO: If not provided, always get a game launcher
+arguments["gamemode"] = "meat_arena"
 
-    # # Initialize the database.
-    # TODO: Move.
-    # from src.lib.util import db
+# Default to not using a save
+arguments["resume"] = False
 
-    # Initialize the game.
-    # TODO: Game chooser?
-    from src.games.meat_arena.meat import Game
-    game = Game(stdscr, resume=False)
+# Parse command line arguments
+try:
+    DEBUG("Parsing command line arguments.")
+    options, remainder = getopt.getopt(sys.argv[1:],"a:b:")
+except getopt.GetoptError:
+    DEBUG("Failed to parse arguments: %s." % sys.argv[1:])
+    exit()
 
-    # Main game loop
-    while game.alive is True:
-        game.loop()
+# TODO: Handle more command line arguments.
+for opt, arg in options:
+    DEBUG("Command line argument '%s': '%s'" % (opt, arg))
 
-# Initialize the curses wrapper, which calls the main function after setting up curses.
-curses.wrapper(main)
+    # The game to be launched
+    if opt in ('-g', '--gamemode'):
+        arguments["gamemode"] = opt
+    # The save to be loaded
+    elif opt in ('-s', '--savefile'):
+        pass
+    # The display flags to apply
+    elif opt in ('-d', '--displayflag'):
+        # Non-recognized flags are a no-op.
+        arguments["displaymode"] |= displayflags.get(opt, 0b0)
+
+# Plain curses mode.
+if arguments["displaymode"] == displayflags["curses"]:
+    import curses
+
+    def main(stdscr):
+        # Set curses options.
+        curses.curs_set(0) # Make the cursor invisible
+        curses.raw() # Use raw input mode.
+
+        # Initialize colors.
+        from src.lib.util.color import Color
+
+        # Initialize the root component.
+        from src.lib.components.component import RootComponent
+        root = RootComponent(stdscr)
+
+        # Get a list of valid game names.
+        choices = []
+        for module_name in sorted(system.folders("src/games")):
+            module_info = __import__('src.games.%s.info' % module_name, globals(), locals(), ['name', 'version', 'description'])
+            choices.append((module_name, module_info))
+
+        # Spawn a game choice menu.
+        from src.lib.components.views.screens.screen import MenuScreen
+        root.spawn(MenuScreen(root.window, **{"title" : "Start Game!", "choices" : choices, "callback" : root.launch}))
+
+        # Start the application loop.
+        while root.alive is True:
+            root.loop()
+
+    # Initialize the curses wrapper, which calls the main function after setting up curses.
+    curses.wrapper(main)
