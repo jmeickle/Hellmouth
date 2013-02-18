2cd76e8678b727a55a6e1cc3d03df5626fad2ec7
Eronarn Palazzo <eronarn@gmail.com>
2013-02-17 21:03:23 -0500

    Cast to string when logging to debug.

--- src/lib/util/debug.py
+++ src/lib/util/debug.py
@@ -2,14 +2,14 @@
 
 import logging
 
-logging.basicConfig(filename='hellmouth.log',level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s',)
+logging.basicConfig(filename='debug.log',level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s',)
 
 # TODO: Use this somewhere.
 debug_frequency = {}
 
 # TODO: Support setting level to info/warning
 def DEBUG(message, level=logging.DEBUG):
-    msg = "%s" % message
+    msg = message.__str__()
     hits = debug_frequency.get(msg, 0) + 1
     debug_frequency[msg] = hits
     logging.debug("%s:%s" % (hits, msg))
