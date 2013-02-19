971825ab9a3d4e6329e119291eb60a37fa0aac34
Eronarn Palazzo <eronarn@gmail.com>
2013-02-18 19:46:48 -0500

    Create a Context class that provides information about the responses available to an event.

---
+++ src/lib/util/context.py
@@ -0,0 +1,16 @@
+"""Temporary objects that determine which responses are available to an event."""
+
+class AgentContext(object):
+    """A simple Context where one Agent responds to an event based on either
+    one or more members or a source."""
+
+    def __init__(self, agent, members, source):
+        self.agent = agent
+        self.members = members
+        self.source = source
+
+    def get_interactions(self):
+        """Return the interactions made available to an Agent by the members of a Context."""
+        for member in self.members:
+            for interaction in member.get_interactions(self.agent, self.source):
+                yield (member, interaction)
