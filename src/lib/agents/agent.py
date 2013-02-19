f565c21423604b117951ca8ad49bf6a942c30189
Eronarn Palazzo <eronarn@gmail.com>
2013-02-18 19:51:23 -0500

    Switch over to a system using events and Contexts, and move some functions around.

--- src/lib/agents/agent.py
+++ src/lib/agents/agent.py
@@ -1,62 +1,53 @@
 """Defines the methods for the basic Agent class."""
 
+from src.lib.util.command import Command
+from src.lib.util.context import AgentContext
 from src.lib.util.dynamic import caller
 from src.lib.util.hex import *
 from src.lib.util.log import Log
 
 class Agent(object):
+    # TODO: What Components do all Actors have?
+    components = []
 
     def __init__(self, components=[]):
-        self.components = []
         self.component_registry = {}
 
-        for component in components + self.components:
+        for component in Agent.components + components:
             self.register_component(component)
 
-    def action(self, methods, action, **kwargs):
-        """Process an action consisting of a sequence of steps required for completion."""
-        # Setup keyword arguments.
-        kwargs = self.prep_kwargs(kwargs)
+    def register_component(self, component, domain=None):
+        """Initializes an instance of a Component class and registers it as the sole member of its domain."""
+        if not domain:
+            domain = component.__name__
+        assert domain not in self.component_registry
+        self.component_registry[domain] = [component(self)]
 
-        # Get the results of processing the action.
-        results = action.process(methods, **kwargs)
+    def retrieve_components(self, domain=None):
+        """Return an iterator over Components (optionally, limited to a domain)."""
+        if domain:
+            for component in self.component_registry.get(domain):
+                yield component
+        else:
+            for domain, components in self.component_registry.items():
+                for component in components:
+                    yield component
 
-        # TODO: Return the full results!
-        # A full attempt returns len(methods) * len(self.definition) results.
-        if len(results) != len(action.sequence) * len(methods):
-            return False
-
-        # If we did reach the end, we only need to check the last primitive.
-        return results[-1][0]
-
-    """Action helper functions - shorthand for calls to self.action()."""
-
-    def believe(self, act, **kwargs):
-        """Check whether an action is believed to be attemptable."""
-        return self.action(["believe"], act, **kwargs)
-
-    def can(self, act, **kwargs):
-        exit(act)
-        """Check whether an action can actually be attempted."""
-        return self.action(["can"], act, **kwargs)
-
-    def attempt(self, act, **kwargs):
-        """Check whether an action can actually be attempted; if so, attempt it."""
-        return self.action(["can", "do"], act, **kwargs)
-
-    # TODO: Implement this properly!
+    # TODO: Handle checking interruptions and the like.
     def process(self, domain, method, *args):
         """Process a method for every Component in a domain.
 
         Every Component in a domain is offered a chance to process the method
         in turn and contribute to the result set. After the last component has
         responded, the order reverses."""
-        implementations = self.component_registry.get(domain)
+        implementations = [component for component in self.retrieve_components(domain)]
         result = None
+
         for implementation in implementations:
             result = implementation.process(method, result, args)
-        for implementation in reversed(implementations):
-            result = implementation.process(method, result, args)
+        # TODO: Attempt to exit early if current result == None?
+#        for implementation in reversed(implementations):
+#            result = implementation.process(method, result, args)
         return result
 
     # TODO: Let implementations override
@@ -79,17 +70,80 @@ class Agent(object):
         if reaction:
             reaction(*args)
 
-    def register_component(self, component, domain=None):
-        """Initializes a Component as the sole member of its domain in the registry."""
-        if not domain:
-            domain = component.__name__
-        assert domain not in self.component_registry
-        self.component_registry[domain] = [component(self)]
+    def get_context(self, members=[], source=None):
+        """Return a Context object containing information used to decide how to process an Agent's event."""
+        assert members or source
+        return AgentContext(self, members, source)
+
+    def respond(self, event, context, scope=None):
+        """Respond to an event occurring within a given Context."""
+        commands = [command for command in self.get_commands()] + [interaction for interaction in context.get_interactions()]
+        for origin, command in self.sort_commands(commands):
+            if event in command.events():
+                # TODO: Make this hurt other people less
+                action = command.get_action()()
+                kwargs = {"target" : origin}
+
+                if not scope:
+                    result = self.attempt(action, **kwargs)
+                else:
+                    result = self.action(scope, action, **kwargs)
+
+                # STUB: need a parse_result function.
+                return result
+        # Whether to continue
+        Log.add("Couldn't do: %s" % event)
+        return True
+
+    def get_commands(self, domain=None):
+        """Query each Component for the commands it makes accessible to an Agent."""
+        return self.process(domain, "get_commands")
+
+    # STUB
+    def sort_commands(self, commands):
+        """Sort the commands accessible to an Agent."""
+        return commands
+
+    def get_interactions(self, agent, source):
+        """List the interaction options exposed to another Agent within a given source."""
+        yield Command("Attack")
 
     # Return your own cell.
+    # TODO: Multi-cell creatures.
     def cell(self):
         return self.map.cell(self.pos)
 
     # Calculate the distance between an actor and a target.
+    # TODO: Multi-cell creatures.
     def dist(self, target):
         return dist(self.pos, target.pos)
+
+    def action(self, scope, action, **kwargs):
+        """Process an action consisting of a sequence of steps required for completion."""
+        # Setup keyword arguments.
+        kwargs = self.prep_kwargs(kwargs)
+
+        # Get the results of processing the action.
+        results = action.process(scope, **kwargs)
+
+        # TODO: Return the full results!
+        # A full attempt returns len(scope) * len(action.sequence) results.
+        if len(results) != len(action.sequence) * len(scope):
+            return False
+
+        # If we did reach the end, we only need to check the last primitive.
+        return results[-1][0]
+
+    """Action helper functions - shorthand for calls to self.action()."""
+
+    def believe(self, action, **kwargs):
+        """Check whether an action is believed to be attemptable."""
+        return self.action(["believe"], action, **kwargs)
+
+    def can(self, action, **kwargs):
+        """Check whether an action can actually be attempted."""
+        return self.action(["can"], action, **kwargs)
+
+    def attempt(self, action, **kwargs):
+        """Check whether an action can actually be attempted; if so, attempt it."""
+        return self.action(["can", "do"], action, **kwargs)
