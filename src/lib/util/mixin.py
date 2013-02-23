b81d20378326f534c9538ba834af20489cc990de
Eronarn Palazzo <eronarn@gmail.com>
2013-02-23 14:11:21 -0500

    Update documentation for Mixin base class.

--- src/lib/util/mixin.py
+++ src/lib/util/mixin.py
@@ -1,4 +1,10 @@
-"""Define a base Mixin class."""
+"""Defines a minimal base class for Mixins."""
+
+"""Usage note: if a Mixin has a prefix like "Un", retain the capitalization of
+the prefixed Mixin. For example, `StoreMixin` should become `UnStoreMixin`.
+This permits more convenient references between these classes.
+"""
 
 class Mixin(object):
+    """Minimal base class for Python multiple inheritance using Mixins."""
     pass
