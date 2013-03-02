0141136fc1fc334efa9a6a8e95cda204d20d2f55
Eronarn Palazzo <eronarn@gmail.com>
2013-03-02 08:52:55 -0500

    Change result objects

--- src/lib/util/result.py
+++ src/lib/util/result.py
@@ -1,16 +1,51 @@
 """Defines a result object."""
 
+import functools
+
+def accumulate_results(fn):
+    """Decorator function for calls that accumulate results."""
+    @functools.wraps(fn)
+    def wrapper(self, *args, **kwargs):
+        results = kwargs.pop('results', None)
+        result = fn(self, *args, **kwargs)
+        if results:
+            results.update(result)
+        return result
+    return wrapper
+
+def ignore_results(fn):
+    """Decorator function for calls that ignore results so far."""
+    @functools.wraps(fn)
+    def wrapper(self, *args, **kwargs):
+        results = kwargs.pop('results', None)
+        return fn(self, *args, **kwargs)
+    return wrapper
+
+def single_results(fn):
+    """Decorator function for methods that won't be called if there is a result already."""
+    @functools.wraps(fn)
+    def wrapper(self, *args, **kwargs):
+        results = kwargs.pop('results', None)
+        if not results:
+            return fn(self, *args, **kwargs)
+        elif results.can_update():
+            result = fn(self, *args, **kwargs)
+            results.update(result)
+            return result
+    return wrapper
+
 class Result(object):
-    def __init__(self, caller, domain, operation, method, args):
+    def __init__(self, caller, domain, method, args=None, kwargs=None):
         self.results = []
         self.caller = caller
         self.domain = domain
-        self.operation = operation
         self.method = method
         self.args = args
+        self.kwargs = kwargs
 
     def update(self, result):
-        self.results.extend(result)
+        self.results.append(result)
+        return True
 
     def describe(self):
         yield "%s called: %s.%s%s:\n" % (self.caller, self.method, self.domain, self.args)
@@ -23,10 +58,12 @@ class Result(object):
             text += line
         return text
 
-    def parse(self):
+    def get_result(self):
         if not self.results:
             return False
-        #exit(self.get_text())
+
+    def get_values(self):
+        return self.results
 
 class CommandResult(Result):
     def __init__(self, caller, command, scope):
@@ -38,7 +75,7 @@ class CommandResult(Result):
     def update(self, result):
         self.results.append(result)
 
-    def parse(self):
+    def get_result(self):
         if not self.results:
             return "failure", False
         else:
@@ -46,3 +83,18 @@ class CommandResult(Result):
                 if not result:
                     return method, result
         return "success", True
+
+class SingleResult(Result):
+    def update(self, result):
+        if not self.results:
+            self.results = result
+            return True
+        return False
+
+    def can_update(self):
+        if not self.results:
+            return True
+        return False
+
+    def get_result(self):
+        return self.results
