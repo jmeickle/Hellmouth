af27a715b7d4df79b55cc48799219d19979f599a
Eronarn Palazzo <eronarn@gmail.com>
2013-03-02 08:52:07 -0500

    Heavy rework to action.py

--- src/lib/agents/components/action.py
+++ src/lib/agents/components/action.py
@@ -6,6 +6,77 @@ permits more convenient references between these classes.
 
 
 from src.lib.util.debug import DEBUG
+from src.lib.util.result import CommandResult
+
+class Action(object):
+    """A sequence of methods to be called in order."""
+
+    def __init__(self, sequence=None):
+        if not sequence:
+            sequence = self.__class__.sequence
+            """Load the default sequence for that class if one is not provided."""
+
+    def process(self, scopes, **kwargs):
+        """Process this Action by executing its primitive sequence within a
+        list of scopes.
+
+        The necessary arguments for each primitive are pulled from the provided
+        keyword arguments. The return value accumulates the method return
+        values into a 'striped' list:
+            [Method1, Method2, Method3] x [Primitive1, Primitive2, Primitive3]
+            == M1P1, M2P1, M3P1, M1P2, M2P2, M3P2, M1P3, M2P3, M3P3
+        
+        If any function returns False, processing will stop, meaning that the
+        return value has variable length."""
+        results = kwargs["results"]
+
+        # For each primitive in this action's definition...
+        for primitive_definition in self.sequence:
+
+            # Get the name of the current primitive.
+            primitive = primitive_definition[0]
+
+            # Get the primitive's desired arguments from the action definition.
+            primitive_args = primitive_definition[1:]
+
+            # Always use actor as the first argument.
+            args = (kwargs["actor"],)
+
+            # Populate the rest of the arguments, if any.
+            for primitive_arg in primitive_args:
+                # We don't do this here - what if an argument really *should*
+                # be none? Leave it up to the function to assert.
+                # assert(kwargs.get(primitive_arg) is not None)
+                args += (kwargs.get(primitive_arg),)
+
+            # Process that primitive within each scope.
+            for scope in scopes:
+                method, result = self.process_primitive(scope, primitive, *args)
+
+                # Handle the case of pure T/F function returns.
+                # TODO: Make all reachable functions return better data rather
+                # than just T/F.
+                # try:
+                #     if result[0]:
+                #         pass
+                # except TypeError:
+                #     result = (result,)
+
+                # Store the function result.
+                results.update((method, result))
+
+                # If the function's primary result was False, exit early.
+                if result is False:
+                    return False#results
+
+        return True#results
+
+    def process_primitive(self, scope, primitive, *args):
+        """Call the primitive's initiator's method within a scope."""
+        method = scope + "_" + primitive
+
+        return method, getattr(args[0], method )(*args[1:])
+
 #
 # ACTION PRIMITIVE CALLBACK METHODS:
 #
@@ -44,116 +115,40 @@ from src.lib.util.debug import DEBUG
 
 # An individual action primitive.
 # TODO: Remove this class; we don't explicitly need an ActionPrimitive class now.
-class ActionPrimitive():
-    # When a primitive is initialized, it generates a class method for each
-    # of its self.methods() according to the pattern in self.add_method(). By
-    # default, these generated class methods call a corresponding actor method
-    # according to the pattern in self.apply().
-    def __init__(self):
-        for method in self.methods():
-            self.add_method(method)
+# class ActionPrimitive():
+#     # When a primitive is initialized, it generates a class method for each
+#     # of its self.methods() according to the pattern in self.add_method(). By
+#     # default, these generated class methods call a corresponding actor method
+#     # according to the pattern in self.apply().
+#     def __init__(self):
+#         for method in self.methods():
+#             self.add_method(method)
 
-    # The methods that can be applied to this primitive.
-    # TODO: Make this a dict containing arguments to check against.
-    def methods(self):
-        return ['believe', 'can', 'do']
+#     # The methods that can be applied to this primitive.
+#     # TODO: Make this a dict containing arguments to check against.
+#     def methods(self):
+#         return ['believe', 'can', 'do']
 
-    # The name of a corresponding actor method for this primitive. By default
-    # it looks for "believe_touch", "can_grasp", etc.
-    def apply(self, method):
-        return method + '_' + self.__class__.__name__
+#     # The name of a corresponding actor method for this primitive. By default
+#     # it looks for "believe_touch", "can_grasp", etc.
+#     def apply(self, method):
+#         return method + '_' + self.__class__.__name__
 
-    # Add a class method to a primitive to check the corresponding actor method.
-    def add_method(self, method):
-        def _default_method(self, actor, *args):
-            result = getattr(actor, self.apply(method), None)(*args)
-            # If we got None as our result, that means the function either
-            # returned nothing or didn't exist in the first place. Log it.
-            if result is None:
-                DEBUG("Actor %s did not have method %s(%s)" % (actor.name, method, args))
-                return (False,)
-            return result
-        _default_method.__name__ = method
-        setattr(self.__class__, _default_method.__name__, _default_method)
+#     # Add a class method to a primitive to check the corresponding actor method.
+#     def add_method(self, method):
+#         def _default_method(self, actor, *args):
+#             result = getattr(actor, self.apply(method), None)(*args)
+#             # If we got None as our result, that means the function either
+#             # returned nothing or didn't exist in the first place. Log it.
+#             if result is None:
+#                 DEBUG("Actor %s did not have method %s(%s)" % (actor.name, method, args))
+#                 return (False,)
+#             return result
+#         _default_method.__name__ = method
+#         setattr(self.__class__, _default_method.__name__, _default_method)
 
-class Action(object):
-    """A sequence of primitives to be called."""
 
-    def __init__(self, sequence=None):
-        if not sequence:
-            sequence = self.__class__.sequence
-            """Load the default sequence for that class if one is not provided."""
-        assert sequence is not None
 
-    def process(self, scopes, **kwargs):
-        """Process this Action by executing its primitive sequence within a
-        list of scopes.
-
-        The necessary arguments for each primitive are pulled from the provided
-        keyword arguments. The return value accumulates the method return
-        values into a 'striped' list:
-            [Method1, Method2, Method3] x [Primitive1, Primitive2, Primitive3]
-            == M1P1, M2P1, M3P1, M1P2, M2P2, M3P2, M1P3, M2P3, M3P3
-        
-        If any function returns False, processing will stop, meaning that the
-        return value has variable length."""
-
-        results = []
-        # For each primitive in this action's definition...
-        for primitive_definition in self.sequence:
-
-            # Get the name of the current primitive.
-            primitive = primitive_definition[0]
-
-            # Get the primitive's desired arguments from the action definition.
-            primitive_args = primitive_definition[1:]
-
-            # Always use actor as the first argument.
-            args = (kwargs["actor"],)
-
-            # Populate the rest of the arguments, if any.
-            for primitive_arg in primitive_args:
-                # We don't do this here - what if an argument really *should*
-                # be none? Leave it up to the function to assert.
-                # assert(kwargs.get(primitive_arg) is not None)
-                args += (kwargs.get(primitive_arg),)
-
-            # Process that primitive within each scope.
-            for scope in scopes:
-                result = self.process_primitive(scope, primitive, *args)
-
-                # Handle the case of pure T/F function returns.
-                # TODO: Make all reachable functions return better data rather
-                # than just T/F.
-                try:
-                    if result[0]:
-                        pass
-                except TypeError:
-                    result = (result,)
-
-                # Store the function result.
-                results.append(result)
-
-                # If the function's primary result was False, exit early.
-                if result[0] is False:
-                    return results
-        return results
-
-    def process_primitive(self, scope, primitive, *args):
-        """Call the primitive's initiator's method within a scope."""
-        return getattr(args[0], scope + "_" + primitive)(*args[1:])
-
-class Attack(Action):
-    """Attack a target with a weapon held in a manipulator."""
-    sequence = [
-        ("touch", "weapon"),
-        ("grasp", "weapon"),
-        # ("lift", "weapon"),
-        # ("handle", "weapon"),
-        ("ready", "weapon"),
-        ("contact", "target", "weapon"),
-        ("use_at", "target", "weapon")
-    ]
 
 #
 # ACTIONS:
@@ -201,28 +196,4 @@ actiondict = {
     "dodge" : (
         ("move", "actor", "pos"),
     ),
-
-    # Use a ready item to parry an attack.
-    # n.b. - The 'target' is the other item!
-    "parry" : (
-        ("touch", "item"),
-        ("grasp", "item"),
-        ("lift", "item"),
-        ("handle", "item"),
-        ("ready", "item"),
-        ("contact", "target", "item"),
-        ("use_at", "target", "item")
-    ),
-
-    # Use a ready item to disarm.
-    # n.b. - The 'target' is the other item!
-    "disarm" : (
-        ("touch", "item"),
-        ("grasp", "item"),
-        ("lift", "item"),
-        ("handle", "item"),
-        ("ready", "item"),
-        ("contact", "target", "item"),
-        ("use_at", "target", "item")
-    ),    
 }
