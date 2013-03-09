1bfc1cde566fc539939a811a52bf9fee2f11a946
Eronarn Palazzo <eronarn@gmail.com>
2013-03-09 08:26:14 -0500

    Add Husk as a game on the startup menu.

---
+++ src/games/husk/data/screens.py
@@ -0,0 +1,12 @@
+text = {
+
+"start" : {
+    "header_left" : "<yellow-black>A desolate cornfield</>",
+    "body_text" : "Hello world!",
+    },
+
+"end" : {
+    "header_left" : "<yellow-black>A desolate cornfield</>",
+    "body_text" : "Goodbye world!",
+    }
+}
---
+++ src/games/husk/info.py
@@ -0,0 +1,6 @@
+# The name of this game.
+name = "Husk"
+# The version number.
+version = "v7DRL.0"
+# The description
+description = "Survival horror roguelike focusing on stealth, allies, and social interaction."
