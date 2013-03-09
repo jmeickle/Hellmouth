139d2f0443b8e7c81916178c83453025bf6d4d4b
Eronarn Palazzo <eronarn@gmail.com>
2013-03-09 09:36:40 -0500

    Add default footers.

--- src/games/husk/data/screens.py
+++ src/games/husk/data/screens.py
@@ -1,3 +1,6 @@
+footer = "Press <green-black>Enter</> to continue. Press <green-black>'?'</> for help at any time."
+footer_back = "Press <green-black>Space</> to go back. Press <green-black>'?'</> for help at any time."
+
 text = {
 
 "start" : {
