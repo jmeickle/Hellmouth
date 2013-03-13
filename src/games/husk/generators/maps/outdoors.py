85675bff8ea3eacf5f4266f13afc09640d9a7922
Eronarn Palazzo <eronarn@gmail.com>
2013-03-13 12:39:40 -0400

    Cornfield generator

---
+++ src/games/husk/generators/maps/outdoors.py
@@ -0,0 +1,39 @@
+from src.lib.generators.maps.mapgen import MapGen
+from src.lib.util.hex import *
+from src.games.husk.agents.terrain.outdoors import Corn
+
+class Cornfield(MapGen):
+    def __init__(self, exits=None):
+        MapGen.__init__(self, exits)
+        self.wall_width = 3
+
+    def attempt(self):
+        hexes = area(self.center, self.size, True)
+
+        # Cornfield edge.
+        for pos, dist in hexes:
+            if dist >= self.size - self.wall_width:
+                self.cells[pos] = (dist, Corn())
+            else:
+                self.cells[pos] = (dist, None)
+
+        hexes = [pos for pos, dist in hexes]
+
+        # Corn rows.
+        spacing = 2 # Every other row
+        pos = self.center
+        for d in [NE, SW]:
+            row_start = add(self.center, d)
+            while row_start in hexes:
+                self.cells[row_start] = (dist, Corn())
+                for row in [NW, SE]:
+                    row_pos = row_start
+                    while row_pos in hexes:
+                        row_pos = add(row, row_pos)
+                        self.cells[row_pos] = (dist, Corn())
+                row_start = add(row_start, mult(d, spacing))
+
+        self.place_exits()
+        self.connect_exits()
+        self.place_levers()
+        return self.cells, self.exits
