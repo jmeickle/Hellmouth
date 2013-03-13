f7a4b8fb74017a600b36437f1342da2bb49c8f35
Eronarn Palazzo <eronarn@gmail.com>
2013-03-13 15:13:07 -0400

    Corn rows have trampled corn in them

--- src/games/husk/agents/terrain/outdoors.py
+++ src/games/husk/agents/terrain/outdoors.py
@@ -6,3 +6,11 @@ class Corn(Terrain):
         self.name = "corn"
         self.glyph = "|"
         self.color = "yellow-black"
+
+class TrampledCorn(Terrain):
+    def __init__(self, terrain_type=None):
+        Terrain.__init__(self)
+        self.name = "trampled corn"
+        self.glyph = ","
+        self.color = "yellow-black"
+        self.blocking = False
--- src/games/husk/generators/maps/outdoors.py
+++ src/games/husk/generators/maps/outdoors.py
@@ -1,6 +1,6 @@
 from src.lib.generators.maps.mapgen import MapGen
 from src.lib.util.hex import *
-from src.games.husk.agents.terrain.outdoors import Corn
+from src.games.husk.agents.terrain.outdoors import Corn, TrampledCorn
 
 class Cornfield(MapGen):
     def __init__(self, exits=None):
@@ -13,7 +13,7 @@ class Cornfield(MapGen):
         # Cornfield edge.
         for pos, dist in hexes:
             if dist >= self.size - self.wall_width:
-                self.cells[pos] = (dist, Corn())
+                self.cells[pos] = (dist, self.place_corn())
             else:
                 self.cells[pos] = (dist, None)
 
@@ -25,15 +25,22 @@ class Cornfield(MapGen):
         for d in [NE, SW]:
             row_start = add(self.center, d)
             while row_start in hexes:
-                self.cells[row_start] = (dist, Corn())
+                self.cells[row_start] = (dist, self.place_corn())
                 for row in [NW, SE]:
                     row_pos = row_start
                     while row_pos in hexes:
                         row_pos = add(row, row_pos)
-                        self.cells[row_pos] = (dist, Corn())
+                        self.cells[row_pos] = (dist, self.place_corn())
                 row_start = add(row_start, mult(d, spacing))
 
         self.place_exits()
         self.connect_exits()
         self.place_levers()
         return self.cells, self.exits
+
+    def place_corn(self):
+        import random
+        if random.choice(range(50)) > 40:
+            return TrampledCorn()
+        else:
+            return Corn()
