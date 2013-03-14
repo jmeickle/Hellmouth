4a831f901889925f466a3d24150d31f7f08dec4e
Eronarn Palazzo <eronarn@gmail.com>
2013-03-14 17:02:46 -0400

    Let crows fly over cornrows

--- src/games/husk/agents/terrain/outdoors.py
+++ src/games/husk/agents/terrain/outdoors.py
@@ -3,14 +3,18 @@ from src.lib.objects.terrain import Terrain
 class Corn(Terrain):
     def __init__(self, terrain_type=None):
         Terrain.__init__(self)
-        self.name = "corn"
+        self.name = "row of corn"
         self.glyph = "|"
         self.color = "yellow-black"
+
+    def can_block(self, agent, direction):
+        if "flight" in agent.get_movement_modes():
+            return False
+        return True
 
 class TrampledCorn(Terrain):
     def __init__(self, terrain_type=None):
         Terrain.__init__(self)
-        self.name = "trampled corn"
+        self.name = "section of trampled corn"
         self.glyph = ","
         self.color = "yellow-black"
-        self.blocking = False
