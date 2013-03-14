d79948e3d3aea89fde957848e8f1f3341644f769
Eronarn Palazzo <eronarn@gmail.com>
2013-03-14 14:49:50 -0400

    Nerf the scarecrow

--- src/games/husk/agents/actors/player.py
+++ src/games/husk/agents/actors/player.py
@@ -1,8 +1,8 @@
 # The player character(s).
-from src.lib.agents.actors.player import PC
+from src.lib.agents.actors.actor import Actor
 from src.lib.agents.components.commander import Commander, CommandingAgent
 
-class Player(PC, CommandingAgent):
+class Player(Actor, CommandingAgent):
     """Default player."""
     def __init__(self, components=[Commander]):
         super(Player, self).__init__(components)
@@ -12,8 +12,8 @@ class Player(PC, CommandingAgent):
         self.description = "A tatterdemalion shape of straw and rags. And... skin?"
         self.highlights = {}
 
-        self.points["traits"]["DX"] += 100
-        self.points["traits"]["ST"] += 100
-        self.build(200)
+        # self.points["traits"]["DX"] += 100
+        # self.points["traits"]["ST"] += 100
+        self.build(50)
         self.recalculate()
         self.controlled = True
