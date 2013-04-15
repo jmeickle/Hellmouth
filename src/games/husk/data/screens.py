acce48ecfc41c5fa981b31b33db601a0d8aff606
Eronarn Palazzo <eronarn@gmail.com>
2013-04-15 01:36:42 -0400

    Add credits to Husk.

--- src/games/husk/data/screens.py
+++ src/games/husk/data/screens.py
@@ -11,5 +11,30 @@ text = {
 "end" : {
     "header_left" : "<yellow-black>A desolate cornfield</>",
     "body_text" : "Goodbye world!",
+    },
+"credits" : {
+    "header_left" : "Thanks for trying out <yellow-black>Husk!</>",
+    "header_right" : "Credits",
+    "body_text" : "\
+Husk is my failed 7DRL from 2013. I built using the framework intended for\
+Hellmouth, a much longer game that I hope to release one day. Your character in\
+Hellmouth is a demonslayer who travels across Medieval Europe in search of \
+Satan's minions. While you may fit the traditional archetype of a \
+paladin blazing with the light of God, demonslayers are a varied lot, and \
+others prefer to dispatch their foes with subterfuge or use their charisma to \
+rally peasant armies. A few dark souls even risk eternal damnation by \
+embracing the corrupt powers of Hell...\
+<br><br>\
+You can follow Hellmouth's development at:\
+<br><br>\
+    http://eronarn.info/tag/hellmouth/ or https://github.com/Eronarn/Hellmouth\
+<br><br>\
+I want to improve Husk because Hellmouth will be based on its code. I'd really\
+appreciate any feedback that you have to offer:\
+<br><br>\
+    https://twitter.com/#!/eronarn or eronarn@gmail.com\
+<br><br>\
+Thanks again for playing!",
+    "footer_text" : "Press <green-black>Enter</> to exit the game.",
     }
 }
