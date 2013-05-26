72fe176ce716404551636b4ced78ebfac5d69565
Eronarn Palazzo <eronarn@gmail.com>
2013-05-26 14:14:33 -0400

    More text helper functions

--- src/lib/util/text.py
+++ src/lib/util/text.py
@@ -4,6 +4,43 @@
 from src.lib.util.define import *
 
 import re
+import functools
+
+def affix(prefix="", suffix=""):
+    """Decorator to add prefixes or suffixes to a string."""
+    def decorator(fn):
+        @functools.wraps(fn)
+        def wrapper(*args, **kwargs):
+            return prefix + fn(*args, **kwargs) + suffix
+        return wrapper
+    return decorator
+
+def indent(fn):
+    """Decorator to handle indenting lines of text."""
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        kwargs.setdefault("indenter", Indenter())
+        result = fn(*args, **kwargs)
+        if isinstance(result, basestring):
+            try:
+                result = kwargs["indenter"].get_indent() + result
+            except AttributeError:
+                exit(kwargs)
+        else:
+            result = [kwargs["indenter"].get_indent() + line for line in result]
+        if kwargs["indenter"].wrap_depth > 0:
+            kwargs["indenter"].unindent().wrap_depth -= 1
+        return result
+    return wrapper
+
+def join(string):
+    """Decorator to join the results of a function by a string."""
+    def decorator(fn):
+        @functools.wraps(fn)
+        def wrapper(*args, **kwargs):
+            return string.join(fn(*args, **kwargs))
+        return wrapper
+    return decorator
 
 def first(character, string):
     """Return the position of the first occurence of a character in a string."""
@@ -94,3 +131,73 @@ def highlight_substr(string, substr, fg=HIGHLIGHT, bg=BACKGROUND):
 def highlight_first(string, fg=HIGHLIGHT, bg=BACKGROUND):
     """Convenience function to highlight the first letter of a string."""
     return highlight(string[0]) + string[1:]
+
+class Indenter(object):
+    def __init__(self, level=0, pattern=" ", width=3, left="", right="", left_partials=True):
+        self.indent_levels = [level]
+        self.wrap_depth = 0
+
+        self.pattern = pattern
+        self.width = width
+        self.left = left
+        self.right = right
+        self.left_partials = left_partials
+
+    def indent(self, levels=1):
+        if levels <= 0: debug.die("Tried to indent 0 or fewer levels.")
+        self.indent_levels.append(levels)
+        return self
+
+    def unindent(self, levels=1):
+        self.indent_levels = self.indent_levels[:-levels]
+        if not self.indent_levels: debug.die("Tried to indent 0 or fewer levels.")
+        return self
+
+    def get_indent_level(self):
+        return sum(self.indent_levels)
+
+    def get_indent(self):
+        indent_level = self.get_indent_level()
+        if indent_level <= 0:
+            return ""
+
+        width = indent_level * self.width - len(self.left) - len(self.right)
+
+        if width < 0:
+            debug.die('Not enough room to generate an indent like: "%s" + "%s" * ~%i + "%s"' % (self.left, self.pattern, width / len(self.pattern), self.right))
+
+        complete_patterns = width / len(self.pattern)
+        remaining_width = width % len(self.pattern)
+
+        return self.left + (self.pattern[:remaining_width] if self.left_partials else "") + (self.pattern * complete_patterns) + (self.pattern[:-remaining_width] if not self.left_partials else "") + self.right
+
+    def wrap_indent(self, levels=1):
+        self.indent(levels)
+        self.wrap_depth += 1
+        return self
+
+def repr_instance(instance, **kwargs):
+    return "<%s at %s>" % (instance.__class__.__name__, hex(id(instance)))
+
+def repr_function(function, **kwargs):
+    try:
+        return "%s.%s()" % ((repr_instance(function.im_self) if hasattr(function, "im_self") else "<unbound>"), \
+            (function.__name__ if hasattr(function, "__name__") else function.__class__.__name__))
+    except AttributeError:
+        debug.die(dir(function))
+
+@affix(prefix="---", suffix="---")
+def repr_header(instance, **kwargs):
+    return repr_instance(instance, **kwargs)
+
+def repr_arguments(pos_args, kw_args, **kwargs):
+    return ["args[%d]: %s" % (i, repr(pos_args[i])) for i in range(len(pos_args))] + ["kwargs[%s]: %s" % (repr(k), repr(v)) for k,v in kw_args.items()]
+
+#@indent
+# def repr_called(caller, called, *args, **kwargs):
+#     result = repr_callable(caller, called)
+#     result += "("
+#     result += ", ".join(args)
+#     result += ", ".join(["%s=%s" % (repr(k), repr(v)) for k,v in kwargs.items()])
+#     result += ")"
+#     return result
