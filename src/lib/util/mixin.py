8fb3740066a66329debffbe2941d623272083412
Eronarn Palazzo <eronarn@gmail.com>
2013-07-04 14:36:13 -0400

    Make Mixin an abstract base class.

--- src/lib/util/mixin.py
+++ src/lib/util/mixin.py
@@ -5,9 +5,11 @@ the prefixed Mixin. For example, `StoreMixin` should become `UnStoreMixin`.
 This permits more convenient references between these classes.
 """
 
+import abc
+
 class Mixin(object):
     """Minimal base class for Python multiple inheritance using Mixins."""
-    pass
+    __metaclass__ = abc.ABCMeta
 
 class DebugMixin(Mixin):
     """Debug Mixin, checked against when enabling debug-only functionality."""
