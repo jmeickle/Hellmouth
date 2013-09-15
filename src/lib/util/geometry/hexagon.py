c272cc9df6a67a79db02ffc12c7e12d3eb378da2
Eronarn Palazzo <eronarn@gmail.com>
2013-09-15 01:54:03 -0400

    Update the hexagon module documentation and imports.

--- src/lib/util/geometry/hexagon.py
+++ src/lib/util/geometry/hexagon.py
@@ -1,8 +1,32 @@
-"""Geometry for hexagons."""
+"""Define the geometry of hexagons.
 
-from src.lib.util.geometry import *
+This module expands the terminology guides in `src.lib.util.geometry.space` 
+and `src.lib.util.geometry.shape` with some definitions specific to hexagons:
 
-class Hexagon(object):
+_heading_: One of the six cardinal hexagonal directions. These are represented
+as the points at a distance of 1 from the coordinate system's origin (0, 0):
+
+       NW    NE
+         /  \
+     CW | CC | CE
+         \  /
+       SW    SE
+
+
+_rank_: The 'radius' of a set of hexagons. Given a set of hexagons and an origin 
+within it, the rank is the greatest distance observed.
+
+_pole_: Given an origin, rank, and heading, the hexagon that would be reached by 
+traveling along a heading until reaching that rank.
+
+_index_: Given an origin, rank, heading, and rotation, an enumeration of the 
+order in which hexagons at that rank are visited.
+"""
+
+from src.lib.util.geometry.shape import Shape
+from src.lib.util.geometry.space import *
+
+class Hexagon(Shape):
     """Class for hexagonal shape."""
 
     # Number of edges
