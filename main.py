e4dae12fe0381e6e5103db48415fc7518a96b7f1
Eronarn Palazzo <eronarn@gmail.com>
2013-09-21 18:46:14 -0400

    Rework main.py to lean on the kernel.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import sys
-import getopt
+from src.lib.core.kernel import Kernel
+from src.lib.components.component import RootComponent
 from src.lib.util import debug
 from src.lib.util.define import *
 from src.lib.util import system
 
 debug.log("Completed all imports.")
 
-# Set up a dictionary of game settings.
-arguments = {}
+# Initialize the Unicursal kernel.
+kernel = Kernel()
 
 # Set a bitmask for game-wide display modes.
 displayflags = {
@@ -17,84 +17,79 @@ displayflags = {
     "256" : 0b1000,
 }
 
+# Set up a dictionary of game settings.
+# TODO: Have the kernel parse this.
+arguments = {}
+
 # Default to displaying with curses
 arguments["displaymode"] = displayflags["curses"]
 
 # Default to launching Meat Arena
 # TODO: If not provided, always get a game launcher
-arguments["gamemode"] = "debug"
+# arguments["gamemode"] = "debug"
 
 # Default to not using a save
 arguments["resume"] = False
 
-# Parse command line arguments
-try:
-    debug.log("Parsing command line arguments.")
-    options, remainder = getopt.getopt(sys.argv[1:],"a:b:")
-except getopt.GetoptError:
-    debug.log("Failed to parse arguments: %s." % sys.argv[1:])
+# Provide a list of module folders
+arguments["module_folders"] = ["src/games"]
 
-# TODO: Handle more command line arguments.
-for opt, arg in options:
-    debug.log("Command line argument '%s': '%s'" % (opt, arg))
+# Initialize the root Component.
+root = RootComponent(kernel)
 
-    # The game to be launched
-    if opt in ('-g', '--gamemode'):
-        arguments["gamemode"] = opt
-    # The save to be loaded
-    elif opt in ('-s', '--savefile'):
-        pass
-    # The display flags to apply
-    elif opt in ('-d', '--displayflag'):
-        # Non-recognized flags are a no-op.
-        arguments["displaymode"] |= displayflags.get(opt, 0b0)
+# Register the root Component with the kernel.
+kernel.register_services(root=root)
 
+# print "Kernel services:", kernel.services
+# print "Do a loop wait"
+# # print "retval:", kernel.service("loop").wait()
+# print "Do a kernel wait"
+# print kernel.wait(root)
+loop = kernel.service("loop")
+loop += root
+
+# Register the keyin service.
+# TODO: Have the arguments and game decide this.
+from src.lib.util.io.keyin import KeyinService
+kernel.register_services(keyin=KeyinService())
+loop = kernel.service("loop")
+loop += kernel.service("keyin")
+
+# Register input and output services.
+# TODO: Have the arguments and game decide this.
 if arguments["displaymode"] == displayflags["curses"]:
     """Launch the game in curses mode."""
-    import curses
+    from src.lib.core.services.curses_io import CursesInputService as input_service, CursesOutputService as output_service
+kernel.register_services(input=input_service(), output=output_service())
+loop = kernel.service("loop")
+loop += kernel.service("input")
+loop += kernel.service("output")
 
-    def main(stdscr):
-        # Set curses options.
-        curses.curs_set(0) # Make the cursor invisible
-        curses.raw() # Use raw input mode.
+# Set game mode if not provided.
+if not arguments.get("gamemode"):
+    def get_choices():
+            """Yield an iterator over game choices."""
+            for module_folder in arguments["module_folders"]:
+                for module_package in sorted(system.folders(module_folder)):
+                    # TODO: Convert module folder into package name via sys directory separator.
+                    yield module_package, __import__('src.games.%s.info' % module_package, globals(), locals(), ['name', 'version', 'description'])
 
-        # Initialize colors.
-        from src.lib.util.color import Color
+    # Create a game choice menu that launches after a selection is made.
+    with kernel.service("output") as display:
+        # Set the root Component's window to the curses display.
+        root.window = display
 
-        # Initialize the root Component.
-        from src.lib.components.component import RootComponent
+        # Create a game choice menu.
+        from src.lib.components.views.screens.screen import MenuScreen
+        choice_menu = MenuScreen(title="Start Game!", choices=list(get_choices()),
+                                choice_formatter=lambda choice: choice[1].name,
+                                callback=root.launch)
 
-        # Define the launching process.
-        def launch(root):
-            if arguments.get("gamemode"):
-                # Launch using the chosen game mode.
-                # TODO: Remove extraneous None
-                root.launch((arguments.get("gamemode"), None))
-            else:
-                # Get a list of valid game modes.
-                choices = []
-                for module_name in sorted(system.folders("src/games")):
-                    module_info = __import__('src.games.%s.info' % module_name, globals(), locals(), ['name', 'version', 'description'])
-                    choices.append((module_name, module_info))
+        # Spawn the game choice menu from the root component.
+        root.spawn(choice_menu)
+else:
+    root.launch((arguments.get("gamemode"), None))
 
-                # Spawn a game choice menu that launches after a selection is made.
-                from src.lib.components.views.screens.screen import MenuScreen
-                root.spawn(MenuScreen(root.window, **{"title" : "Start Game!", "choices" : choices, "callback" : root.launch}))
-
-            # Start the root Component's loop.
-            while root.alive is True:
-                root.loop()
-
-            # Return information about desired post-loop behavior.
-            return root.after_loop()
-
-        # Create a RootComponent and then launch it. Repeat this process until
-        # a RootComponent indicates that it is time to stop.
-        while True:
-            root = RootComponent(stdscr)
-            status = launch(root)
-            if status.get("relaunch") is False:
-                break
-
-    # Initialize the curses wrapper, which calls the main function after setting up curses.
-    curses.wrapper(main)
+# Start the main game loop.
+while kernel.loop():
+    pass
