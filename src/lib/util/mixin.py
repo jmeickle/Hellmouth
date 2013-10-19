8c8314908adb0b2f743cd34bd42eb3e0df17587b
Eronarn Palazzo <eronarn@gmail.com>
2013-10-19 14:59:39 -0400

    Update documentation and remove a redundant mixin.

--- src/lib/util/mixin.py
+++ src/lib/util/mixin.py
@@ -1,16 +1,7 @@
-"""Defines a minimal base class for Mixins."""
-
-"""Usage note: if a Mixin has a prefix like "Un", retain the capitalization of
-the prefixed Mixin. For example, `StoreMixin` should become `UnStoreMixin`.
-This permits more convenient references between these classes.
-"""
+"""A minimal abstract base class for Mixins."""
 
 import abc
 
 class Mixin(object):
-    """Minimal base class for Python multiple inheritance using Mixins."""
+    """A minimal abstract base class for Python multiple inheritance via Mixins."""
     __metaclass__ = abc.ABCMeta
-
-class DebugMixin(Mixin):
-    """Debug Mixin, checked against when enabling debug-only functionality."""
-    pass
