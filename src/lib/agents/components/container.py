efd30d278707866615b2a3e1606fb9ab9cca3049
Eronarn Palazzo <eronarn@gmail.com>
2013-12-27 13:23:37 -0500

    Add a Container trait and remove some old comments.

--- src/lib/agents/components/container.py
+++ src/lib/agents/components/container.py
@@ -3,7 +3,9 @@
 from src.lib.agents.components.component import Component
 
 from src.lib.util import debug
+from src.lib.util.registry import RegistryFactory, RegistryDict, RegistryList
 from src.lib.util.result import accumulate_results, ignore_results
+from src.lib.util.trait import Trait
 
 """Components."""
 
@@ -65,130 +67,90 @@ class Containing(Component):
         """Return the number of Agents inside this Container."""
         return len([agent for agent in self.get_list()])
 
-    # Not currently working:
-    # @ignore_results
-    # def get(self, key, default=None):
-    #     """Default get method."""
-    #     # results = [res for res in self.get_contents()]
-    #     # if results:
-    #     #     exit("key %s, dict %s" % (key, results))
-    #     for appearance, itemlist in self.get_contents():
-    #         for item in itemlist:
-    #             if key == item:
-    #                 return True
-    #     return False
+"""Traits."""
 
-#    def get_matches(self, appearance):
-#        for appearance, itemlist in self.contents.items():
-#            yield appearance
-#            appearance
-#            append = True
-#            for item in itemlist:
-#                if item.is_equipped() is True:
-#                    append = False
-#            if append is True:
-#                items.append((appearance, itemlist))
-#        return sorted(items, key=itemgetter(0))
+class ContainerException(Exception):
+    """An exception raised while using an `Agent` as a `Container`."""
+    pass
 
-# TODO: Use database tables!
-# On import, try to create the necessary database tables.
-#from src.lib.util import db
-# db.query("CREATE TABLE IF NOT EXISTS item (iid int, PRIMARY KEY (iid))")
-# db.query("CREATE TABLE IF NOT EXISTS container (aid int, root int, depth int, PRIMARY KEY (aid))")
-# db.query("CREATE TABLE IF NOT EXISTS container_item (aid int, iid int)")
-# db.query("CREATE TABLE IF NOT EXISTS item_tag (iid int, tid int, detail int, tag text)")
+class CouldNotContainException(ContainerException):
+    """An exception raised when a `Container` tries to add something it could not contain."""
+    pass
 
-# TODO: Check item memory!
-# # Decorator that forces a function to check an actor's item memory.
-# def checks_item_memory(fn):
-#     def wrapped(self, *args):
-#         if hasattr(self, 'item_memory'):
-#             known = (self.item_memory.get(item, Item()),) + args[1:]
-#         return fn(self, *args)
-#     return wrapped
+class CanNotContainException(ContainerException):
+    """An exception raised when a `Container` tries to add something it cannot currently contain."""
+    pass
 
-# Decorator testing.
-# if __name__ == '__main__':
+class DoesNotContainException(ContainerException):
+    """An exception raised when a `Container` tries to remove something it does not currently contain."""
+    pass
 
-    # def checks_knowledge(fn):
-    #     def wrapped(self, *args):
-    #         if hasattr(self, 'complicated'):
-    #             args = ("complication",) + args[1:]
-    #         return fn(self, *args)
-    #     return wrapped
+# A registry for the contents of a Container.
+ContentRegistry = RegistryFactory("ContentRegistry", RegistryList)
 
-    # class Simple:
-    #     def test(self, tag):
-    #         print "Hi! %s" % tag
+class Container(Trait):
+    """Provides methods to manage contents."""
+    contents_class = ContentRegistry
 
-    # class Complicated(Simple):
+    @Trait.include
+    def __init__(self, *args, **kwargs):
+        """Set self's contents."""
+        Trait.super(Container, self).__init__(*args, **kwargs)
+        self.contents = kwargs.pop("contents", [])
 
-    #     @checks_knowledge
-    #     def test(self, tag):
-    #         print "Hiiii! %s" % tag
+    @property
+    def contents(self):
+        return self._contents
 
-    # a = Simple()
-    # b = Complicated()
-    # a.test("br")
-    # b.test("br")
-    # b.complicated = True
-    # b.test("br")
+    @contents.setter
+    def contents(self, values):
+        self._contents = self.contents_class(values) if values is not None else contents_class()
 
-#for run in range(len(run_results)):
-#    t1, t2, num_items = run_results[run]
+    """Content validation methods."""
 
+    def could_contain(self, other):
+        """Return whether self could contain other."""
+        return True
 
-    # tag_enums = ("color", "shape", "size", "weight", "material")
-    # tag_values = (
-    #     ["red", "blue", "green"],
-    #     ["sphere", "cube", "tetrahedron"],
-    #     ["tiny", "average", "big"],
-    #     ["light", "normal", "heavy"],
-    #     ["wood", "metal", "bone"]
-    #     )
+    def can_contain(self, other):
+        """Return whether self can currently contain other."""
+        return True
 
-    # import random
-    # import time
+    def does_contain(self, other):
+        """Return whether self currently contains other."""
+        return other in self.contents
 
-    # run_results = []
-    # runs = 100
-    # for run in range(runs):
-    #     t1 = time.time()
+    """Content modification methods."""
 
-    #     # Make database
+# A registry for sections within a SectionedContainer.
+SectionRegistry = RegistryFactory("SectionRegistry", RegistryList)
 
+# A registry for the contents of sections within a SectionedContainer.
+SectionContentRegistry = RegistryFactory("SectionContentRegistry", RegistryDict, container_class=RegistryList)
 
+class SectionedContainer(Container):
+    contents_class = SectionContentRegistry
 
+    @Trait.include
+    def __init__(self, *args, **kwargs):
+        """Set self's contents."""
+        self.sections = kwargs.pop("sections")
+        Trait.super(SectionedContainer, self).__init__(*args, **kwargs)
 
-    #     # Set up inventories
-    #     for i in range(20):
-    #         conn.execute("INSERT INTO container(aid, root, depth) VALUES (?, ?, ?)", (i, None, None))
+    @property
+    def sections(self):
+        return self._sections
 
-    #     num_items = 1000
-    #     for i in range(num_items):
-    #         conn.execute("INSERT INTO item(iid) VALUES (?)", (i,))
-    #         conn.execute("INSERT INTO container_item(aid, iid) VALUES (?, ?)", (random.choice(range(20)), i))
+    @sections.setter
+    def sections(self, values):
+        self._sections = SectionRegistry()
 
-    #         num_tags = 1 + random.choice(range(4))
-    #         for tag in range(num_tags):
-    #             tid = random.choice(range(5))
-    #             lod = random.choice(range(3))
-    #             tag_text = random.choice(tag_values[tid])
-    #             conn.execute("INSERT INTO item_tag(iid, tid, detail, tag) VALUES (?, ?, ?, ?)", (i, tid, lod, tag_text))
-
-    #     txt = random.choice(tag_values[random.choice(range(5))])
-    #     results = conn.execute('''SELECT item.iid, item_tag.tid, item_tag.tag FROM item
-    #         LEFT JOIN container_item ON item.iid == container_item.iid
-    #         LEFT JOIN container ON container.aid == container_item.aid
-    #         LEFT JOIN item_tag ON item.iid == item_tag.iid WHERE item_tag.tag = ? AND item_tag.detail < ? AND item.iid < ?''', (txt, random.choice((1,2,3)), random.choice(range(num_items))))
-    #     num_results = 0
-    #     for result in results:
-    #         num_results += 1
-    #     #for iid, tid, tag in results:
-    #         #= result
-    #         # print "Item #%s has '%s: %s'." % (iid, tag_enums[tid], tag)
-
-    #     conn.close()
-    #     t2 = time.time()
-    #     run_results.append((t1,t2, num_results))
-    #     print 'Run %s: retrieved %s items in %0.3f ms' % (run+1, num_results, (t2-t1)*1000.0)
+    # TODO: Test whether this works
+    @Container.contents.setter
+    def contents(self, values):
+        self._contents = self.contents_class()
+        for value in values:
+            section = self.choose_section(value)
+            if not section in self.sections:
+                raise CouldNotContainException, "There was no appropriate section for that content."
+            self._contents[section] += value
