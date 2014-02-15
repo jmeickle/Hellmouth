fb8d654665d9a461f89311a86c4116fb483a6c85
Eronarn Palazzo <eronarn@gmail.com>
2014-02-14 22:47:20 -0500

    Convert the Service class into being a non-trait.

--- src/lib/core/services/service.py
+++ src/lib/core/services/service.py
@@ -1,13 +1,7 @@
 """Services registered to the kernel."""
 
-from src.lib.util.trait import Trait
-
-class Service(Trait):
+class Service(object):
     """Provides methods to classes that are registered as kernel services."""
-
-    def __init__(self, *args, **kwargs):
-        self.kernel = None
-        Trait.super(Service, self).__init__(*args, **kwargs)
 
     def __repr__(self):
         """Display as `<ClassName>` when printed."""
