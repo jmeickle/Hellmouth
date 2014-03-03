111de30e95e0b78837e026612fde61cfc2892cce
Eronarn Palazzo <eronarn@gmail.com>
2014-03-03 15:38:50 -0500

    Update draw() to take a display.

--- src/lib/components/views/screens/help.py
+++ src/lib/components/views/screens/help.py
@@ -6,15 +6,15 @@ class HelpScreen(Screen):
         super(HelpScreen, self).__init__(**kwargs)
 
     # TODO: Header drawing should be nicer.
-    def draw(self):
-        self.border(" ")
+    def draw(self, display):
+        display.border(self, " ")
         title = "Help!"
         heading = "<green-black>%s</>" % title
-        self.cline(heading)
-        self.cline("-"*(self.width))
-        self.cline(help.entry["commands"])
-        self.y_acc = self.BOTTOM
-        self.cline(screens.footer)#_back)
+        display.cline(self, heading)
+        display.cline(self, "-"*(self.width))
+        display.cline(self, help.entry["commands"])
+        self.y_acc = self.bottom
+        display.cline(self, screens.footer)
 
     def keyin(self, c):
         if c == ord('\n'):
