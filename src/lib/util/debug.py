51caf3ff23365ab80e04151735a39790a19aa618
Eronarn Palazzo <eronarn@gmail.com>
2014-03-03 16:21:55 -0500

    Adjust debug logging to include file, line number, and function.

--- src/lib/util/debug.py
+++ src/lib/util/debug.py
@@ -1,6 +1,7 @@
 """Debugging and system logging methods and decorators."""
 
 import functools
+import inspect
 import logging
 
 logging.basicConfig(filename='debug.log',level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s',)
@@ -9,16 +10,22 @@ logging.basicConfig(filename='debug.log',level=logging.DEBUG, format='%(asctime)
 # TODO: Use this somewhere.
 debug_frequency = {}
 
+def describe_call(skip_calls=1):
+    """Given a number of calls on the stack to skip, return a previous call's
+    module, method, and line number.
+    """
+    call = inspect.stack()[skip_calls+1]
+    _, module, line, method, _, _ = call
+    return module.rpartition("src")[2], method, line
+
 # TODO: Support setting level to info/warning
 def log(message, **kwargs):
     """Send a message to the debug log."""
     level = kwargs.pop("level", logging.DEBUG)
     msg = message.__str__()
-    if kwargs.pop("line", False):
-        import inspect
-        last_call = inspect.stack()[1]
-        _, module, line, method, _, _ = last_call
-        msg = "{}({}) in {}: {}".format(module.partition("src")[2], line, method, msg)
+    if kwargs.pop("line", True):
+        module, method, line = describe_call(1)
+        msg = "{}({}) in {}: {}".format(module, line, method, msg)
     hits = debug_frequency.get(msg, 0) + 1
     debug_frequency[msg] = hits
     logging.log(level, "%s:%s" % (hits, msg))
@@ -28,10 +35,6 @@ def die(message, **kwargs):
     kwargs.setdefault("level", logging.CRITICAL)
     log(message, **kwargs)
     assert False, "\n{}".format(message)
-
-def lineno():
-    """Returns the current line number."""
-    return inspect.currentframe().f_back.f_lineno
 
 def DEBUG(fn):
     """Decorator to log a method call to debug."""
